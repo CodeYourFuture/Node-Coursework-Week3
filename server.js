c34f40747af59b304e1bc917717602a347558d61
Chris Owen <cowen1992@hotmail.co.uk>
2020-06-07 14:55:26 +0100

    Add files

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//Use this array as your (in-memory) data store.
+const bookings = require("./bookings.json");
+
+app.get("/", function (request, response) {
+  response.send("Hotel booking server.  Ask for /bookings, etc.");
+});
+
+// TODO add your routes and helper functions here
+
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
