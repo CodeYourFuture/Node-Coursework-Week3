7eba9d7502df5c26c1d341d0a479a29b4064e8a8
mahmutsel <mahmutsel29@gmail.com>
2020-07-06 15:21:21 +0100

    level-1-completed

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+let bookings = require("./bookings.json");
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
@@ -15,6 +14,49 @@ app.get("/", function (request, response) {
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+//Read all bookings
+app.get("/bookings", (req, res) => {
+  res.send(bookings);
+});
+
+//Read one booking, specified by an ID
+app.get("/bookings/:id", (req, res) => {
+  const selectedBooking = bookings.filter(
+    (booking) => booking.id === Number(req.params.id)
+  );
+  if (selectedBooking) {
+    res.send(selectedBooking);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+//Create a new booking
+app.post("/bookings", (req, res) => {
+  console.log(req.body);
+  if (req.body !== "") {
+    bookings.push(req.body);
+    res.send({ success: true });
+  } else {
+    res.sendStatus(404);
+  }
+  res.send();
+});
+
+//Delete a booking, specified by an ID
+app.delete("/bookings/:id", (req, res) => {
+  const deleteBooking = bookings.filter(
+    (booking) => booking.id !== Number(req.params.id)
+  );
+  if (bookings.length !== deleteBooking.length) {
+    bookings = deleteBooking;
+    res.send(bookings);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+const myPort = process.env.PORT || 3003;
+app.listen(myPort, () => {
+  console.log(` Your app is listening on port ${myPort}`);
 });
