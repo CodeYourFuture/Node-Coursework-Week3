05950495c4ef447bb59a389e61636cd88854700a
byohannes <42332389+byohannes@users.noreply.github.com>
2020-07-07 12:42:31 +0100

    createing new bookings and validations are done

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
@@ -7,21 +7,54 @@ app.use(express.json());
 app.use(cors());
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+const bookings = require('./bookings.json');
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+app.get('/', function (request, response) {
+  response.send('Hotel booking server.  Ask for /bookings, etc.');
 });
 
-// TODO add your routes and helper functions here
+// Read all bookings
 
-app.get("/bookings", function (request, response) {
+app.get('/bookings', function (request, response) {
   response.json(bookings);
 });
 
+const validateId = (id) => {
+  return bookings.some((booking) => booking.id === id);
+};
 
-const portNumber=process.env.PORT||5000;
+const validateRequest = (request) => {
+  return (
+    request.body.title === '' ||
+    request.body.firstName === '' ||
+    request.body.surname === '' ||
+    request.body.email === '' ||
+    request.body.roomId === '' ||
+    request.body.checkInDate === '' ||
+    request.body.checkOutDate === ''
+  );
+};
+//  Create a new bookings
+app.post('/bookings', (request, response) => {
+  const newId = bookings.length + 1;
+  const newIdExist = validateId(newId);
+  if (newIdExist) {
+    newId++;
+  }
+  const requestValidated = validateRequest(request);
+  if (requestValidated) {
+    response.status(400).json('Please Fill all the form fields, thanks!');
+  } else {
+    request.body.id = newId;
+    bookings.push(request.body);
+    response
+      .status(201)
+      .json(` Successfully A new chat with Id number ${newId}  is created.`);
+  }
+});
+
+const portNumber = process.env.PORT || 5000;
 
 const listener = app.listen(portNumber, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });
