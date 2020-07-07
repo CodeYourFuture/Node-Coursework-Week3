779a76c80b77e8e1b9b5487fcd9e88ca9c7317be
byohannes <42332389+byohannes@users.noreply.github.com>
2020-07-07 13:09:52 +0100

    read a booking by an Id and deleteing a booking are done

--- server.js
+++ server.js
@@ -53,6 +53,27 @@ app.post('/bookings', (request, response) => {
   }
 });
 
+//  Read one booking specified by an ID
+app.get('/bookings/:id', (request, response) => {
+  const {id} = request.params;
+  const booking = bookings.find((element) => element.id === Number(id));
+  booking ? response.json(booking) : response.sendStatus(404);
+});
+
+//  Delete a booking by ID
+app.delete('/bookings/:id', (request, response) => {
+  const {id} = request.params;
+  const indexToDelete = bookings.findIndex(
+    (booking) => booking.id === Number(id)
+  );
+  if (indexToDelete != -1) {
+    bookings.splice(indexToDelete, 1);
+    response.sendStatus(204);
+  } else {
+    response.sendStatus(404);
+  }
+});
+
 const portNumber = process.env.PORT || 5000;
 
 const listener = app.listen(portNumber, function () {
