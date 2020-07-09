84857d40197853174205c9fe9b91a69c3ef4e6e8
SERKAN CAVUS <40465585+SCavus@users.noreply.github.com>
2020-07-10 00:53:13 +0100

    level-3 complete

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const moment = require('moment')
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,12 +13,27 @@ app.use(bodyParser.json());
 //Use this array as your (in-memory) data store.
 let bookings = require("./bookings.json");
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+app.get("/", function (req, res) {
+  res.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
 app.get("/bookings", (req, res) => {
   res.json(bookings);
+});
+
+// checkInDate": "2018-02-15",
+// "checkOutDate": "2018-02-28"
+app.get("/bookings/search", (req, res) => {
+  const date = req.query.date;
+  console.log(date)
+  const searchDate = bookings.filter(
+    (booking) => booking.checkInDate === date || booking.checkOutDate === date
+  );
+  if (searchDate.length > 0) {
+    res.json(searchDate);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.get("/bookings/:bookingId", (req, res) => {
@@ -42,14 +58,17 @@ app.post("/bookings", (req, res) => {
   } else {
     res.send("Please fill the form");
   }
-})
+});
 
 app.delete("/bookings/:id", (req, res) => {
   const bookingId = Number(req.params.id);
-  const checkLength = bookings.length
-  bookings = bookings.filter(booking => booking.id != bookingId);
-  if (checkLength > bookings.length) {res.json({ "Booking deleted": true })}
-  else {res.sendStatus(404)};
+  const checkLength = bookings.length;
+  bookings = bookings.filter((booking) => booking.id != bookingId);
+  if (checkLength > bookings.length) {
+    res.json({ "Booking deleted": true });
+  } else {
+    res.sendStatus(404);
+  }
 });
 // TODO add your routes and helper functions here
 
