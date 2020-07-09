92b3b55ef8e324210b11c6c4670245980ebe3e06
ferhatu <ferhat_2001@yahoo.com>
2020-07-09 15:31:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -35,11 +35,11 @@ app.get("/bookings/:id", (req, res) => {
 });
 
 //delete by Id
-app.delete("/bookings/:id", (req, res) =>{
+app.delete("/bookings/:id", (req, res) => {
   const bookingId = Number(req.params.id);
-  bookings = bookings.filter(item => item.id !== personId);
-  res.send({ success: true })
-})
+  bookings = bookings.filter((item) => item.id !== bookingId);
+  res.send({ success: true });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
