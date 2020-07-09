d435f7a1452fc2a986e8a71cec7904dceacd7769
ferhatu <ferhat_2001@yahoo.com>
2020-07-09 15:07:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,18 @@ app.get("/", function (request, response) {
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+//read all messages
+app.post("/bookings", (req, res) => {
+  bookings.push(req.body);
+  res.send({ success: true });
 });
+app.get("/bookings", function (request, response) {
+  response.send(bookings);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+// const listener = app.listen(process.env.PORT, function () {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
