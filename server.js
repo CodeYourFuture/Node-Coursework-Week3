e75bf849c0f6e0f58fe98226319c8a27364c541c
Nader Akhgari <nader90akhgari@gmail.com>
2020-08-06 18:50:24 +0100

    Changed the endpoint to show all bookings from node.js to mongodb

--- server.js
+++ server.js
@@ -1,132 +1,38 @@
 const express = require("express");
+const mongodb = require('mongodb');
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const moment = require("moment");
 const validator = require("email-validator");
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.json());
 app.use(cors());
 
-//Use this array as your (in-memory) data store.
-let bookings = require("./bookings.json");
+const uri = process.env.DB_URI;
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+const option = { useUnifiedTopology: true }
+const client = new mongodb.MongoClient(uri, option)
+client.connect(()=>{
+  const db = client.db("hotel-server");
+  const collection = db.collection("bookings");
+
+  app.get("/", function (req, res) {
+    res.send("Hotel booking server.  Ask for /bookings, etc.");
+  });
+
+  // TODO add your routes and helper functions here
+app.get("/bookings", (req, res) => {
+  collection.find().toArray((err,results)=>{
+    if(err){
+      res.status(500).send(err)
+    }else{
+      res.status(200).send(results)
+    }
+  })
+});
 });
 
-// TODO add your routes and helper functions here
-app.get("/bookings", (request, response) => {
-  response.send(bookings);
-});
-
-app.post("/bookings/add", (req, res) => {
-  const {
-    title,
-    firstName: firstName,
-    surname,
-    email,
-    roomId,
-    checkInDate,
-    checkOutDate,
-  } = req.body;
-  const isEmailOk = validator.validate(email);
-  const isDateOk = moment(checkInDate) < moment(checkOutDate) ? true : false;
-
-  if (
-    title &&
-    firstName &&
-    surname &&
-    isEmailOk &&
-    isDateOk &&
-    roomId &&
-    checkInDate &&
-    checkOutDate
-  ) {
-    const booking = {
-      id: Math.floor(Math.random() * 1000000),
-      title: title,
-      firstName: firstName,
-      surname: surname,
-      email: email,
-      roomId: roomId,
-      checkInDate: checkInDate,
-      checkOutDate: checkOutDate,
-    };
-    bookings.push(booking);
-    res.send(bookings);
-  } else {
-    res.status(400).send("check the inputs, something is wrong ! ");
-  }
-});
-
-app.get("/bookings/search", (req, res) => {
-  const date = req.query.date;
-  const term = req.query.term;
-  const searchedBooking = bookings.filter(
-    (booking) =>
-      booking.checkInDate === date ||
-      booking.checkOutDate === date ||
-      booking.firstName.toLowerCase().includes(term.toLowerCase()) ||
-      booking.surname.toLowerCase().includes(term.toLowerCase()) ||
-      booking.email.toLowerCase().includes(term.toLowerCase())
-  );
-  if (searchedBooking.length > 0) {
-    res.send(searchedBooking);
-  } else {
-    res.status(400).send("Not found!");
-  }
-});
-
-app.put("/bookings/:id", (req, res) => {
-  const bookingId = Number(req.params.id);
-  const found = bookings.some((booking) => booking.id === bookingId);
-  if (found) {
-    const booking = bookings.find((booking) => booking.id === bookingId);
-    const {
-      title,
-      firstName,
-      surname,
-      email,
-      checkInDate,
-      checkOutDate,
-    } = req.body;
-    booking.title = title ? title : booking.title;
-    booking.firstName = firstName ? firstName : booking.firstName;
-    booking.surname = surname ? surname : booking.surname;
-    booking.email = email ? email : booking.email;
-    booking.roomId = roomId ? roomId : booking.roomId;
-    booking.checkInDate = checkInDate ? checkInDate : booking.checkInDate;
-    booking.checkOutDate = checkOutDate ? checkOutDate : booking.checkOutDate;
-    res.json(bookings);
-  } else {
-    res.status(404).send("not found!");
-  }
-});
-
-app.delete("/bookings/:id", (req, res) => {
-  const bookingId = Number(req.params.id);
-  const found = bookings.some((booking) => booking.id === bookingId);
-  if (found) {
-    bookings = bookings.filter((booking) => booking.id !== bookingId);
-    res.json(bookings);
-  } else {
-    res.status(404).send("not found");
-  }
-});
-
-app.get("/bookings/:id", (req, res) => {
-  const bookingId = Number(req.params.id);
-  const booking = bookings.find((booking) => booking.id === bookingId);
-  if (Object.entries(booking).length > 0) {
-    res.send(booking);
-  } else {
-    res.status(404).send("not found");
-  }
-});
-
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> console.log("Your app is listening on port " + port));
