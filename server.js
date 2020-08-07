631ed91a4a24cfe3046134b35b425b2daf195e6a
Nader Akhgari <nader90akhgari@gmail.com>
2020-08-07 01:06:11 +0100

    changed the endpoint to create a new bookings from nodejs to mongodb

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 const cors = require("cors");
-const moment = require("moment");
+const dayjs = require("dayjs");
 const validator = require("email-validator");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
@@ -12,9 +12,9 @@ app.use(cors());
 
 const uri = process.env.DB_URI;
 
-const option = { useUnifiedTopology: true }
-const client = new mongodb.MongoClient(uri, option)
-client.connect(()=>{
+const option = { useUnifiedTopology: true };
+const client = new mongodb.MongoClient(uri, option);
+client.connect(() => {
   const db = client.db("hotel-server");
   const collection = db.collection("bookings");
 
@@ -22,17 +22,61 @@ client.connect(()=>{
     res.send("Hotel booking server.  Ask for /bookings, etc.");
   });
 
-  // TODO add your routes and helper functions here
-app.get("/bookings", (req, res) => {
-  collection.find().toArray((err,results)=>{
-    if(err){
-      res.status(500).send(err)
-    }else{
-      res.status(200).send(results)
+  app.get("/bookings", (req, res) => {
+    collection.find().toArray((err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(results);
+      }
+    });
+  });
+
+  app.post("/bookings/add", (req, res) => {
+    const {
+      title,
+      firstName,
+      surname,
+      email,
+      roomId,
+      checkInDate,
+      checkOutDate,
+    } = req.body;
+    const isEmailOk = validator.validate(email);
+    const isDateOk = dayjs(checkInDate) < dayjs(checkOutDate) ? true : false;
+
+    if (
+      !title ||
+      !firstName ||
+      !surname ||
+      !isEmailOk ||
+      !isDateOk ||
+      !roomId ||
+      !checkInDate ||
+      !checkOutDate
+    ) {
+      return res
+        .status(400)
+        .send(" something is wrong! check the inputs and try again ");
     }
-  })
-});
+    const booking = {
+      title: title,
+      firstName: firstName,
+      surname: surname,
+      email: email,
+      roomId: roomId,
+      checkInDate: checkInDate,
+      checkOutDate: checkOutDate,
+    };
+    collection.insertOne(booking, (err, result) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(result.ops[0]);
+      }
+    });
+  });
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=> console.log("Your app is listening on port " + port));
+app.listen(port, () => console.log("Your app is listening on port " + port));
