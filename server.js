ca5a738e7fe1f2b8f060b295f04b0a6f1207dfe3
F Nawal Abdul Careem <nawal@fs-macbook-air.home>
2020-09-06 22:31:42 +0100

    added functionality read all bookings

--- server.js
+++ server.js
@@ -9,12 +9,20 @@ app.use(cors());
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+
+app.get("/",  (req, res) => {
+  res.send("Hotel booking server.  Ask for /bookings, etc.");
 });
+
+
+//Read all bookings
+app.get("/bookings", (req, res) => {
+  res.json(bookings);
+});
+
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3001, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
