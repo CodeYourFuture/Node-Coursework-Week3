6a207a9be4b6586b2737f421121961a30ce5a8b0
Osman Elsahib <elsahib10@gmail.com>
2020-09-06 21:59:34 +0100

    end of level 1

--- server.js
+++ server.js
@@ -14,7 +14,54 @@ app.get("/", function (request, response) {
 });
 
 // TODO add your routes and helper functions here
+app.post("/bookings", function (request, response) {
+  const expectedKeys = ["title", "firstName", "surname", "email", "roomId", "checkInDate", "checkOutDate"]
+  for (let item in request.body) {
+    if (!request.body[item] || !expectedKeys.includes(item)) {
+      response.status(400).json("Please ensure your are passing the correct fileds")
+      return;
+    }
+  }
 
-const listener = app.listen(process.env.PORT, function () {
+  let newBooking = {
+    id: bookings[bookings.length - 1].id + 1,
+    title: request.body.title,
+    firstName: request.body.firstName,
+    surname: request.body.surname,
+    email: request.body.email,
+    roomId: request.body.roomId,
+    checkInDate: request.body.checkInDate,
+    checkOutDate: request.body.checkOutDate
+  }
+  bookings.push(newBooking)
+  response.json(bookings)
+});
+
+app.get("/bookings", function (request, response) {
+  response.send(bookings);
+});
+
+app.get("/bookings/:id", function (request, response) {
+  const id = parseInt(request.params.id)
+  let requestedBooking = bookings.find(booking => booking.id === id)
+  if (requestedBooking) {
+    response.json(requestedBooking)
+  } else {
+    response.status(404).json("There is no booking with this ID");
+
+  }
+});
+app.delete("/bookings/:id", function (request, response) {
+  const id = parseInt(request.params.id)
+  let requestedBooking = bookings.findIndex(booking => booking.id === id)
+  if (requestedBooking > -1) {
+    bookings.splice(requestedBooking, 1)
+    response.json(bookings)
+  } else {
+    response.status(404).json("There is no booking with this ID");
+  }
+});
+
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
