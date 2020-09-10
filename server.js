eb85868f12a30a617c3a23fcb3f342f48c170109
Leida_Gandy <leida.gandy@gmail.com>
2020-09-10 18:23:03 +0100

    Get One booking specified by Id

--- server.js
+++ server.js
@@ -21,6 +21,22 @@ app.get("/bookings", (request, response) => {
   response.json(bookings);
 });
 
+// 2.Read one booking, specified by Id
+
+app.get("/bookings/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const idSearched = bookings.filter((booking) => booking.id === id);
+  const found = bookings.some((booking) => booking.id === id);
+
+  if (found) {
+    response.json(idSearched);
+  } else {
+    response
+      .status(404)
+      .send(`No bookings match the id ${id}. Please enter a valid Id.`);
+  }
+});
+
 const listener = app.listen(process.env.PORT || 5000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
