ca184d472a5f7e9bc520b2baabc89fdb5f367a91
Halit <60817499+halitos@users.noreply.github.com>
2020-09-10 23:58:36 +0100

    lvl3 complete

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const moment = require("moment");
+const { body, validationResult } = require("express-validator");
 
 const app = express();
 
@@ -22,48 +23,50 @@ app.get("/bookings", (req, res) => {
   res.json(bookings);
 });
 
+// *******Search booking with time span******
+
+app.get("/bookings/search", (req, res) => {
+  let date = req.query.date;
+  let matchedBookings = bookings.filter(
+    (book) => book.checkInDate <= date && book.checkOutDate >= date
+  );
+  if (matchedBookings.length > 0) {
+    res.json(matchedBookings);
+  } else res.send("No bookings found matching the chosen timespan");
+});
+
 // **** Create new booking *****
 
 app.post("/bookings", (req, res) => {
+  let checkInFormat = moment(req.body.checkInDate, "YYYY-MM-DD");
+  let checkOutFormat = moment(req.body.checkOutDate, "YYYY-MM-DD");
   let newBooking = {
-    id: bookings.length + 1, // temp solution for now to match with other id, I can use uuid for bigger pros
+    //id: bookings.length + 1, // temp solution for now to match with other id, I can use uuid for bigger pros
     title: req.body.title,
     firstName: req.body.firstName,
     surname: req.body.surname,
     email: req.body.email,
     roomId: req.body.roomId,
-    checkInDate: req.body.checkInDate,
-    //checkInDate: moment().format("L"),
-    checkOutDate: req.body.checkOutDate,
+    checkInDate: checkInFormat,
+    checkOutDate: checkOutFormat,
   };
 
-  if (
+  if (checkInFormat > checkOutFormat) {
+    res.send("Check-out Date must be later than Check-in Dates");
+  } else if (
     newBooking.title.length > 0 &&
     newBooking.firstName.length > 0 &&
     newBooking.surname.length > 0 &&
     newBooking.email.length > 0 &&
     typeof newBooking.roomId == "number" &&
-    newBooking.checkInDate.length > 0 &&
-    newBooking.checkOutDate.length > 0
+    checkInFormat > 0 &&
+    checkOutFormat
   ) {
     bookings.push(newBooking);
     res.json(bookings);
   } else {
     res.status(400).send("Please fill all required properties to book a place");
   }
-
-  //----could not make these methods work----- :(
-
-  // let bookKeys = Object.keys(newBooking);
-  // let checkKeys = bookKeys.filter((p) => p.length > 0);
-  // let emptyProps = bookKeys.find((p) => p.length < 0);
-
-  // if (emptyProp) {
-  //   res.status(400).send("Please fill all required properties to book a place");
-  // } else {
-  //   bookings.push(newBooking);
-  //   res.json(bookings);
-  // }
 });
 
 // ******** Get one by id *******
@@ -93,22 +96,6 @@ app.delete("/bookings/:id", (req, res) => {
       .send(`Please enter id number between 1 and ${bookings.length}`);
   }
 });
-
-// Search booking with time span
-
-app.get("/bookings", (req, res) => {
-  // let startTime = req.body.checkInDate
-  // let endTime = req.body.checkOutDate
-  let date = req.query.date;
-  let matchedBookings = bookings.map(
-    (book) => book.checkInDate <= date && book.checkOutDate >= date
-  );
-  if (matchedBookings !== undefined) {
-    res.json(matchedBookings);
-  } else res.send("No bookings found matching the chosen timespan");
-});
-
-// const port = process.env.PORT || 7070
 
 const listener = app.listen(process.env.PORT || 7070, function () {
   console.log("Your app is listening on port " + listener.address().port);
