e8322306ffce377b73df9c84e9ab179b47381941
Michael <antenehnigussie1@yahoo.com>
2021-05-02 14:20:44 +0100

    Completed Level 1 of the challenge.

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
+const { response } = require("express");
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
@@ -15,6 +16,52 @@ app.get("/", function (request, response) {
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 5000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+/**** LEVEL 1 SOLUTION CODE ****/
+
+app.use(express.json());
+// CREATE new booking
+app.post("/bookings", (req, res) => {
+  // **NOTE**: INPUT VALIDATION HAS TO BE DONE BY THE CLIENT
+  const newId = bookings[bookings.length - 1].id + 1;
+  const newBooking = { id: newId, ...req.body };
+  bookings.push(newBooking);
+  res.sendStatus(201);
+});
+
+// RETRIEVE all bookingS
+app.get("/bookings", (req, res) => {
+  res.status(200).send(bookings);
+});
+
+// RETRIEVE a booking by id
+app.get("/bookings/:id", (req, res) => {
+  const booking = bookings.find(
+    (booking) => booking.id === parseInt(req.params.id)
+  );
+  if (booking) {
+    // if requested booking exists
+    res.status(200).send(booking);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+// DELETE a booking by id
+app.delete("/bookings/:id", (req, res) => {
+  const index = bookings.findIndex(
+    (booking) => booking.id === parseInt(req.params.id)
+  );
+  if (index >= 0) {
+    // if requested booking exists
+    bookings.splice(index, 1);
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+/**** END OF LEVEL 1 SOLUTION ****/
