daafafe65f81254dd9de6fdb7838c50a872844c8
jon <jonprosser84@gmail.com>
2021-05-03 19:19:41 +0100

    started hotel server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
@@ -13,8 +13,13 @@ app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
+//gets all the bookings
+app.get("/bookings", (req, res) => {
+  res.status(200).send(bookings);
+})
+
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 5000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
