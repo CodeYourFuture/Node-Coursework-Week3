dcb5922cd51d2ae04b05ca6035161fd1ea004273
Amanuel <weditedi25@gmail.com>
2021-05-04 18:59:51 +0100

    initial commit and package installation

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get("/", function (request, response) {
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
