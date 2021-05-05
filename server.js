021f0d318e15f37a9c09e430c040817b1c465aad
Amanuel <weditedi25@gmail.com>
2021-05-05 19:02:55 +0100

    creating and reading done

--- server.js
+++ server.js
@@ -13,12 +13,45 @@ app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
+// post method, adding new element
+app.post("/bookings", function (request, response) {
+  let newBooking = request.body;
+
+  if (!newBooking.id) {
+    response.status(400);
+    response.send("Booking Id required");
+  } else if (bookings.find((booking) => booking.id === newBooking.id)) {
+    response.status(400);
+    response.send("booking already exists");
+  } else {
+    bookings.push(newBooking);
+    response.status(201);
+    console.log(newBooking);
+    response.send(newBooking);
+  }
+});
+
+// Reading all bookings
 app.get("/bookings", function (request, response) {
-  response.json(bookings);
+  response.send(bookings);
+});
+
+// Reading a booking by ID
+app.get("/bookings/:id", function (request, response) {
+  // type conversion to int from string
+  let id = parseInt(request.params.id);
+  console.log(id);
+  let filteredBooking = bookings.find((booking) => booking.id === id);
+
+  // if no booking found
+  if (!filteredBooking) {
+    response.sendStatus(404);
+  }
+  response.send(filteredBooking);
 });
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
