63f0b2dd45194a22845adb099b3ce6faf4c4ef64
ltteh <67978926+ltteh@users.noreply.github.com>
2021-05-06 19:22:54 +0100

    Get all bookings and get booking by Id

--- server.js
+++ server.js
@@ -14,7 +14,26 @@ app.get("/", function (request, response) {
 });
 
 // TODO add your routes and helper functions here
+// get all bookings
+app.get("/bookings", (req, res) => {
+  res.send(bookings);
+})
 
-const listener = app.listen(process.env.PORT, function () {
+// get booking by id
+app.get("/bookings/:id", (req, res) => {
+  
+  const bookingId = bookings.find((elem) => (elem.id) == (req.params.id));
+    
+  if(bookingId) {
+    res.status(200).send({
+    booking:  bookingId});
+  } else {
+    res.status(404).send({
+      message: "Booking's Id not found"
+    })
+  }
+})
+
+const listener = app.listen(process.env.PORT || 3001, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
