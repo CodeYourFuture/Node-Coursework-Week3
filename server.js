f041040130df4c739be1194fd791d592fef14fa2
Miss <emesquitta73@gmail.com>
2021-05-06 20:16:40 +0100

    added comments

--- server.js
+++ server.js
@@ -13,21 +13,23 @@ app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
+//to make new bookings
 app.post("/bookings", (req, res) => {
   const newBooking = req.body;
   bookings.push(newBooking);
   res.json(newBooking);
-
 });
 
 app.get("/bookings", (req, res) => res.json(bookings));
 
+// to recognise bookings by ID
 app.get("/bookings/:id", (req, res) => {
-  let id  = parseInt(req.params.id);
+  let id = parseInt(req.params.id);
   const filteredBookings = bookings.find((booking) => booking.id === id);
   res.json(filteredBookings);
 });
 
+//to delete bookings
 
 app.delete("/bookings/:id", (req, res) => {
   let id = parseInt(req.params.id);
@@ -36,12 +38,10 @@ app.delete("/bookings/:id", (req, res) => {
       bookings.splice(booking, 1);
       res.send("booking deleted");
     } else {
-    res.status("400").send("Bad Request");
+      res.status("400").send("Bad Request");
     }
-    
   });
-  });
-
+});
 
 // TODO add your routes and helper functions here
 
