fefefeebab2b7218a3b06565cea3afd7e9774069
ltteh <67978926+ltteh@users.noreply.github.com>
2021-05-06 22:24:42 +0100

    create (post) new booking

--- server.js
+++ server.js
@@ -17,23 +17,52 @@ app.get("/", function (request, response) {
 // get all bookings
 app.get("/bookings", (req, res) => {
   res.send(bookings);
-})
+});
 
 // get booking by id
 app.get("/bookings/:id", (req, res) => {
-  
-  const bookingId = bookings.find((elem) => (elem.id) == (req.params.id));
-    
-  if(bookingId) {
+  const bookingId = bookings.find((elem) => elem.id == req.params.id);
+  // validation
+  if (bookingId) {
     res.status(200).send({
-    booking:  bookingId});
+      bookingId,
+    });
   } else {
     res.status(404).send({
-      message: "Booking's Id not found"
-    })
+      message: "Booking's Id not found",
+    });
   }
-})
+});
 
-const listener = app.listen(process.env.PORT || 3001, function () {
+// create new booking
+app.post("/bookings", (req, res) => {
+  let newId = bookings.length + 1; // auto generate new Id
+
+  const newBooking = {
+    id: newId,
+    ...req.body,
+  };
+
+  // validation
+  if (
+    newBooking.title === "" &&
+    newBooking.firstName === "" &&
+    newBooking.surname === "" &&
+    newBooking.email === "" &&
+    newBooking.roomId === "" &&
+    newBooking.checkInDate === "" &&
+    newBooking.checkoutDate === ""
+  ) {
+    res.status(400);
+    res.send({ message: "Booking Error" });
+  } else {
+    bookings.push(newBooking);
+    res.status(201);
+    res.send(newBooking);
+  }
+});
+
+
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
