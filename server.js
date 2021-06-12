324f2957eb93a80ce19ec7e610f127cb2dc70403
ybayraktutan <74928622+ybayraktutan@users.noreply.github.com>
2021-06-12 12:45:21 +0100

    Level 1,2 completed
    Level 1,2 completed

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const moment = require('moment');
+// moment().format(YYYY-MM-DD); 
 
 const app = express();
 
@@ -8,13 +10,76 @@ app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
+const { response } = require("express");
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
 // TODO add your routes and helper functions here
+app.get("/bookings", (req, res) => {
+  res.send(bookings);
+});
 
+app.post("/bookings", (req, res) => {
+  const newBooking = req.body;
+  newBooking.id = bookings.length + 1 ; 
+  if(!req.body.title ||
+     !req.body.firstName || 
+     !req.body.surname || 
+     !req.body.email || 
+     !req.body.roomId || 
+     !req.body.checkInDate || 
+     !req.body.checkOutDate
+     ) {
+    res.status(400);
+    res.send("You need to fill all the fields");
+  } else {
+    bookings.push(newBooking);
+    res.status(201);
+    res.send(bookings);
+  }  
+})
+
+
+app.get("/bookings/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const requestedBooking = bookings.find(booking => booking.id === id)
+  if(!requestedBooking) {
+    res.status(404).send(`Booking with ID: ${id} not found`);
+  } 
+    res.status(200).json(requestedBooking);
+});
+
+app.delete("/bookings/:id", (req, res) => {
+  const id = Number(req.params.id);
+  let deletedBooking;
+  bookings.map(booking => {
+  if(booking.id === id) {
+    bookings.splice(id, 1);
+    deletedBooking = booking;
+  }   
+  })
+  if(!deletedBooking) {
+    res.status(404);
+    console.log(id);
+    res.send(`Booking with ID: ${id} has not found`);
+  } else {
+    res.status(200);
+    console.log(deletedBooking);
+    res.send(`Booking with ID: ${id} has been deleted`);
+  }
+});
+
+const PORT = 3000;
+
+const listener = app.listen(PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+/*
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+*/
