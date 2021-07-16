8a62d2200c56159516127412aded4406be54ea3c
JarrodBen <jarrodb.247@gmail.com>
2021-07-16 13:36:29 +0200

    initial commit and added a route

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
+const { req, res } = req("express");
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+app.get("/", (req, res) => {
+  res.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
 // TODO add your routes and helper functions here
+
+app.get("bookings", (req, res) => {
+  res.json(bookings)
+});
 
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
