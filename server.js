1ac583a996fe124dff3d3ba536ac295b5c4362ca
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-07-21 00:02:48 +0200

    Level 1 Completed
    Create a new booking.

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const Joi = require("joi");
+const moment = require("moment");
+
 
 const app = express();
 
@@ -9,13 +12,42 @@ app.use(cors());
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
 
-app.get("/", function (request, response) {
+app.get("/",  (request, response) => {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+// get all bookings
+
+  app.get("/bookings", (req, res) => {
+    res.status(200);
+    res.send(bookings);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const listener = app.listen(process.env.PORT || 3000,   () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
