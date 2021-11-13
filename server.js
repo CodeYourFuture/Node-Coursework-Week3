a1efc0dcce32d9082c27a8225055ad648623fe89
Mya <myaskylar14@gmail.com>
2021-11-13 18:44:12 +0000

    level 1 done

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 
 app.use(express.json());
@@ -8,12 +7,71 @@ app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
+const { request, response } = require("express");
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
 // TODO add your routes and helper functions here
+//read all bookings
+app.get("/bookings", (request, response) => {
+  response.send(bookings);
+  console.log("all booking");
+});
+
+//create new booking
+app.post("/bookings", (request, response) => {
+  //get data from the client
+  const booking = request.body;
+
+  const newBooking = {
+    id: bookings[bookings.length -1].id + 1,
+    title: booking.title,
+    firstName: booking.firstName,
+    surname: booking.surname,
+    email: booking.email,
+    roomId: booking.roomId,
+    checkInDate: booking.checkInDate,
+    checkOutDate: booking.checkOutDate,
+  };
+
+  if (booking) {
+    bookings.push(newBooking);
+    response.status(201).send(newBooking);
+  } else {
+    response.status(400).send("error");
+  }
+});
+
+// Read one booking, specified by an ID
+app.get("/bookings/:id", (request, response) => {
+  const filterBooking = bookings.filter(
+    (booking) => booking.id === Number(request.params.id)
+  );
+
+   if (filterBooking.length === 0) {
+     response.status(404).send("Booking cannot be found!:{");
+     return;
+   }
+  response.send(filterBooking);
+});
+
+//Delete a booking, specified by an ID
+app.delete("/bookings/:id", (request, response) => {
+  const index = bookings.findIndex(
+    (booking) => booking.id === Number(request.params.id)
+  );
+
+  if (index === -1) {
+    response.status(404).send("Booking for deletion cannot be found!:{");
+    return;
+  }
+
+  bookings.splice(index, 1);
+console.log(index);
+  response.send("Successfully Deleted!!!");
+});
 
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
