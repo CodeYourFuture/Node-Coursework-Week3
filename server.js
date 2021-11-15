f7228bff4df04d87023a35a1d4737ade8bb15fa9
musayuksel <musayuxel@gmail.com>
2021-11-15 11:35:09 +0000

    feat: Add gitignore,nodemon,port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -10,11 +11,16 @@ app.use(cors());
 const bookings = require("./bookings.json");
 
 app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+  response.send(
+    "Hotel booking server.  Ask for /bookings, etc."
+  );
 });
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+const listener = app.listen(PORT, function () {
+  console.log(
+    "Your app is listening on port " +
+      listener.address().port
+  );
 });
