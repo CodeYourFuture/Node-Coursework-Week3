cec02bbcb61728ad84112c4f374d56266128d69b
musayuksel <musayuxel@gmail.com>
2021-11-15 11:59:11 +0000

    feat: Add Read one booking, specified by an ID

--- server.js
+++ server.js
@@ -20,8 +20,20 @@ app.get("/", function (request, response) {
 app.get("/bookings", (request, response) => {
   response.send(bookings);
 });
+//get one booking by id
+app.get("/bookings/:id", (request, response) => {
+  const customerId = +request.params.id;
+  const customerWithId = bookings.find(
+    (booking) => booking.id === customerId
+  );
+  customerWithId
+    ? response.send(customerWithId)
+    : response.status(404).send({
+        msg: `Custumer doesn't exist with id: ${customerId} `,
+      });
+});
 
-const listener = app.listen(PORT, function () {
+const listener = app.listen(PORT, () => {
   console.log(
     "Your app is listening on port " +
       listener.address().port
