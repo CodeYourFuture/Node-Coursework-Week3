016620ebb55d1943f6386cd39232e2f955051441
omerffaruk <farukyamak11044@gmail.com>
2021-11-17 11:30:21 +0000

    level 2 - simple validation completed

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 //Use this array as your (in-memory) data store.
@@ -20,9 +21,26 @@ app.get('/bookings', (req, res) => {
 })
 
 app.post('/bookings', (req, res) => {
-  const newBooking = req.body;
-  bookings.push(newBooking);
-  res.send(bookings);
+  const lastIndex = bookings.length - 1;
+  const lastId = bookings[lastIndex].id;
+  const booking = req.body;
+  const notValidBooking = !booking.title || !booking.roomId || !booking.firstName || !booking.surname || !booking.email || !booking.checkInDate || !booking.checkOutDate;
+  if(notValidBooking) {
+    res.sendStatus(400);
+  }else {
+    const newBooking = {
+      "id": lastId + 1,
+      "roomId": booking.roomId,
+      "title": booking.title,
+      "firstName": booking.firstName,
+      "surname": booking.surname,
+      "email": booking.email,
+      "checkInDate": booking.checkInDate,
+      "checkOutDate": booking.checkOutDate
+    }
+    bookings.push(newBooking);
+    res.send(bookings);
+  }
 })
 
 app.get('/bookings/:bookingId', (req, res) => {
