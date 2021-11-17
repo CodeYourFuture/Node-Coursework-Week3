306f251d8b8ba23a85a19fc11e8f823068cb2816
omerffaruk <farukyamak11044@gmail.com>
2021-11-17 09:49:25 +0000

    completed level 1 challenge - make the booking server

--- server.js
+++ server.js
@@ -15,6 +15,38 @@ app.get("/", function (request, response) {
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+app.get('/bookings', (req, res) => {
+  res.send(bookings);
+})
+
+app.post('/bookings', (req, res) => {
+  const newBooking = req.body;
+  bookings.push(newBooking);
+  res.send(bookings);
+})
+
+app.get('/bookings/:bookingId', (req, res) => {
+  const bookingId = parseInt(req.params.bookingId);
+  const booking = bookings.find(({id}) => id == bookingId);
+  if(booking === undefined) {
+    res.sendStatus(404);
+    return;
+  }
+  res.send(booking);
+})
+
+
+app.delete('/bookings/:bookingId', (req, res) => {
+  const bookingId = req.params.bookingId;
+  const bookingIndex = bookings.findIndex(({ id }) => id == bookingId);
+  if(bookingIndex === -1) {
+    res.sendStatus(404);
+    return;
+  }
+  bookings.splice(bookingIndex, 1);
+  res.send(bookings);
+})
+
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
