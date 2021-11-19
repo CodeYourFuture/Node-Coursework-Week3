6d1298d6d83b074d4cf4b94a3f0666a5ee7b5968
Maziarmajd <83129200+Maziarmajd@users.noreply.github.com>
2021-11-19 14:20:37 +0000

    level 1+2

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const Joi = require("joi");
+const moment = require("moment");
+
 
 const app = express();
 
@@ -9,12 +12,96 @@ app.use(cors());
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
 
-app.get("/", function (request, response) {
+app.get("/",  (request, response) => {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
-// TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+// get all bookings
+
+  app.get("/bookings", (req, res) => {
+    res.status(200);
+    res.send(bookings);
+  });
+
+// Get one booking by id
+app.get("/bookings/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const foundBooking = bookings.find((booking) => {
+    return booking.id === id;
+    // returns element || undefined
+  });
+
+  foundBooking !== undefined
+    ? res.status(200).send(foundBooking)
+    : res.status(404).send(`There is no booking with id ${id}`);
+});
+
+
+// Create a new booking
+  app.post("/bookings", (req, res) => {
+    const schema = Joi.object({
+      Title: Joi.string().min(2).required(),
+      FirstName: Joi.string().max(50).required(),
+      Surname: Joi.string().max(50).required(),
+      Email: Joi.string().email().required(),
+      RoomId: Joi.number().integer().min(1).required(),
+      CheckInDate: Joi.date().iso().required(),
+      CheckOutDate: Joi.date().iso().greater(Joi.ref("CheckInDate")).required(),
+    });
+
+    const result = schema.validate(req.body);
+
+    const {
+      Title,
+      FirstName,
+      Surname,
+      Email,
+      RoomId,
+      CheckInDate,
+      CheckOutDate,
+    } = req.body;
+
+    const generateRandomId = () => Date.now(); // id for the new booking
+
+    const newBooking = {
+      id: generateRandomId(),
+      Title,
+      FirstName,
+      Surname,
+      Email,
+      RoomId,
+      CheckInDate,
+      CheckOutDate,
+    };
+
+    if (result.error) {
+      res.status(400).send(result.error.details[0].message);
+    } else {
+      bookings.push(newBooking);
+      res.status(200).send(bookings);
+    }
+  });
+
+
+// Delete a booking by id
+app.delete("/bookings/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const bookingToDelete = bookings.findIndex((booking) => {
+    return booking.id === id;
+   
+  });
+
+  if (bookingToDelete !== -1) {
+    bookings.splice(bookingToDelete, 1);
+    res.status(200).send(`Deleted booking with id: ${id}`);
+  } else {
+    res.status(404).send("Not found");
+  }
+});
+
+
+
+const listener = app.listen(process.env.PORT || 3000,   () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
