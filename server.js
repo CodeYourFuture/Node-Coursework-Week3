d0fcfa5114c5701fc552b95a53553413a74cf4f7
Ivan Zhukov <zhkvivan@gmail.com>
2021-12-23 16:25:05 +0000

    bookings/search is not working

--- server.js
+++ server.js
@@ -1,20 +1,85 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const uuid = require('uuid');
+const moment = require('moment');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+const PORT = process.env.PORT || 3001;
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+let bookings = require('./bookings.json');
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+app.get('/', function (request, response) {
+	response.send('Hotel booking server.  Ask for /bookings, etc.');
+});
+
+app.get('/bookings', (req, res) => {
+	res.status(200).send(bookings);
+});
+
+app.post('/bookings', (req, res) => {
+	if (
+		req.body.title.length === 0 ||
+		req.body.firstName.length === 0 ||
+		req.body.surname.length === 0 ||
+		req.body.email.length === 0 ||
+		req.body.roomId.length === 0 ||
+		req.body.checkInDate.length === 0 ||
+		req.body.checkOutDate.length === 0
+	) {
+		res.status(400).send('Please fill out all the fields');
+	} else {
+		bookings.push({
+			id: uuid.v4(),
+			title: req.body.title,
+			firstName: req.body.firstName,
+			surname: req.body.surname,
+			email: req.body.email,
+			roomId: req.body.roomId,
+			checkInDate: req.body.checkInDate,
+			checkOutDate: req.body.checkOutDate,
+		});
+		res.status(201).send(bookings);
+	}
+});
+
+app.get('/bookings/:id', (req, res) => {
+	const id = req.params.id;
+	let requestedPerson = bookings.find((booking) => booking.id == id);
+	if (requestedPerson) {
+		res.status(200).send(requestedPerson);
+	} else {
+		res.status(404).end();
+	}
+});
+
+app.delete('/bookings/:id', (req, res) => {
+	const id = +req.params.id;
+	let newBookings = bookings.filter((booking) => booking.id !== id);
+	if (newBookings.length === bookings.length) {
+		res.status(404).send('There is no bookings with such ID');
+	} else {
+		bookings = newBookings;
+		res.status(200).send(bookings);
+	}
+});
+
+app.get('/bookings/search', (req, res) => {
+	// let date = req.query.date;
+	console.log('ss');
+	res.status(200).send('s');
+});
+app.get('/bookings/a', (req, res) => {
+	// let date = req.query.date;
+	console.log('ss');
+	res.status(200).send('s');
 });
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+const listener = app.listen(PORT, function () {
+	console.log('Your app is listening on port ' + listener.address().port);
 });
