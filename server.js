d596a5f89a8732331cc1d929798641bc5f5b982e
Johng117 <83654331+Johng117@users.noreply.github.com>
2022-01-12 20:42:48 +0000

    Added delete path and altered validation

--- server.js
+++ server.js
@@ -8,18 +8,21 @@ app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
-let bookingNum = bookings.length+1;
+let bookingNum = bookings.length + 1;
 
-
+// root
 app.get("/", (req, res) => {
   res.json("Hotel booking server.  Ask for /bookings, etc.");
 });
+
 
 // get all bookings
 app.get("/bookings", (req, res) => {
   res.json(bookings);
 });
 
+
+// adds a new booking
 app.post("/bookings", (req, res) => {
   const newBooking = {
     id: bookingNum,
@@ -27,17 +30,25 @@ app.post("/bookings", (req, res) => {
     firstName: req.body.firstName,
     surname: req.body.surname,
     email: req.body.email,
-    roomId: req.body.roomId,
+    roomId: parseInt(req.body.roomId),
     checkInDate: req.body.checkInDate,
-    checkOutDate: req.body.checkOutDate
-  }
+    checkOutDate: req.body.checkOutDate,
+  };
   bookingNum++;
-  bookings.push(newBooking);
+  !newBooking.title ||
+  !newBooking.firstName ||
+  !newBooking.surname ||
+  !newBooking.email ||
+  !newBooking.roomId ||
+  !newBooking.checkInDate ||
+  !newBooking.checkOutDate
+    ? res.status(400).json({ msg: "Booking information incomplete" })
+    : bookings.push(newBooking);
   res.status(200).json({
     msg: `Booking made on ${moment().format("MMMM Do YYYY, h:mm:ss a")}`,
   });
-  
 });
+
 
 // get a booking by its id
 app.get("/bookings/:id", (req, res) => {
@@ -47,6 +58,23 @@ app.get("/bookings/:id", (req, res) => {
     ? res.status(200).json(bookingId)
     : res.status(404).json({ msg: "Booking not found" });
 });
+
+
+// deletes a booking
+app.delete("/bookings/:id", (req, res) => {
+  const deletedId = req.params.id;
+  console.log(deletedId);
+  const deletedBooking = bookings.filter((booking) => {
+    return booking.id === parseInt(deletedId);
+  });
+  console.log(deletedBooking);
+  deletedBooking.length === 0
+    ? res.status(404).json({ msg: "Booking not found" })
+    : bookings.splice(bookings.indexOf(deletedBooking[0]), 1);
+  res.json({ msg: `Booking ${deletedId} deleted.` });
+});
+
+
 
 const listener = app.listen(3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
