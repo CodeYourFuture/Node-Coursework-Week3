8043f19aa54e108e1fd84d60829581f3d49be56e
Katie <professionalkateg@gmail.com>
2022-01-14 13:13:23 +0000

    added all the paths needed, now moving onto advanced challanges

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const bookings = require("./bookings.json");
 
 /* sends a response based on the length of the passed booking array
   @param {array} booking - filtered booking array derived from bookings 
+  @param {object} res - the response object returned from the app HTTP methods
   @returns {undefined} */
 const sendResponse = function (booking, res) {
   booking.length === 0
@@ -56,6 +57,51 @@ app.get("/bookings/:id", (req, res) => {
   sendResponse(filteredBooking, res);
 });
 
-const listener = app.listen(3000, () => {
+// delete a booking based on ID
+app.delete("/bookings/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const foundIndex = bookings.findIndex((booking) => booking.id === id); // returns -1 if the ID isn't found
+
+  if (foundIndex === -1)
+    return res
+      .status(404)
+      .json({ success: false, message: "Booking not found" });
+
+  const removedBooking = bookings.splice(foundIndex, 1);
+  res.json({
+    success: true,
+    "removed booking": removedBooking,
+    bookings: bookings,
+  });
+});
+
+// create a new booking
+app.post("/bookings", (req, res) => {
+  const payloadData = req.body;
+
+  for (let key in payloadData) {
+    if (typeof payloadData[key] === "number") {
+      if (!payloadData[key]) {
+        return res.status(404).json({
+          success: false,
+          message: "Cannot make new booking as you are missing some data",
+        });
+      }
+    } else if (!payloadData[key].trim()) {
+      return res.status(404).json({
+        success: false,
+        message: "Cannot make new booking as you are missing some data",
+      });
+    }
+  }
+
+  bookings.push({
+    id: bookings.length === 0 ? 1 : bookings[bookings.length - 1].id + 1,
+    ...payloadData,
+  });
+  res.json({ success: true, bookings: bookings });
+});
+
+const listener = app.listen(3001, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
