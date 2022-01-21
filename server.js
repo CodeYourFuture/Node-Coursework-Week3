a411a69281236efa09c31471acf28494908e1b87
gynaa78 <gynaa78@yahoo.com>
2022-01-21 23:12:30 +0000

    level 1 W-I-P

--- server.js
+++ server.js
@@ -13,8 +13,59 @@ app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
-// TODO add your routes and helper functions here
-
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+app.get ("/bookings", function ( request ,response){
+response.json(bookings);
 });
+
+app.get("/bookings/bookingId", function (request, response){
+  const bookingId = request.params.bookingId;
+  const booking = bookings.find(function (booking) {
+    return booking.bookingId === bookingId;
+      response.json(booking[0]);
+  });
+
+  app.put("/bookings/;bookingId", function (request, response) {
+    bookings.forEach((el, index) => {
+      if (el.id == request.params.bookingId) {
+        let newBooking = { ...el, ...request.body };
+        bookings[index] = newBooking;
+      }
+    });
+    response.json({ success: true });
+  });
+
+// TODO add your routes and helper functions here
+let Id = 1;
+app.post("/bookings", (request, response) => {
+  const bookings = {};
+  booking.id = Id;
+  booking.from = request.body.from;
+  booking.text = request.body.text;
+
+  if (booking.from != " " && booking.text != "") {
+    booking.push(booking);
+    Id = Id + 1;
+    save();
+    response.json({
+      status: "success",
+      booking: "request.body",
+    });
+  } else {
+    response.json(404);
+  }
+});
+
+// app.get("/bookings/:bookingId", function (request, response) {
+//   let booking = bookings.filter((booking) => booking.id == request.params.bookingId);
+
+//   response.json(booking[0]);
+// });
+
+
+
+// If the booking to be read cannot be found by id, return a 404.
+
+// If the booking for deletion cannot be found by id, return a 404.
+
+// All booking content should be passed as JSON.
+app.listen(process.env.PORT)});
