995a2fe91fac76ae56436c1d0cc72f2943efe2bf
AbiyaY <abiyay30@gmail.com>
2022-02-03 13:51:28 +0000

    Level 1 Completed

--- server.js
+++ server.js
@@ -15,6 +15,44 @@ app.get("/", function (request, response) {
 
 // TODO add your routes and helper functions here
 
+app.get("/bookings", (req, res) => {
+  res.json(bookings);
+})
+
+app.get("/bookings/:id", (req, res) => {
+  const bookingId = parseInt(req.params.id);
+  const booking = bookings.find(booking => booking.id === bookingId);
+  if(booking) {
+    res.status(200).send(booking);
+  } else {
+    res.status(404).send({booking: "User not found"});
+  }
+})
+
+app.delete("/bookings/:id", (req, res) => {
+ const bookingId = parseInt(req.params.id);
+ let bookingIndex = bookings.findIndex(booking => booking.Id === bookingId);
+ bookings.splice(bookingIndex, 1);
+ res.send();
+})
+
+
+app.post('/bookings', (req, res) => {
+  const newBooking = {
+    id: bookings.length+1,
+    title: req.body.title,
+    firstName: req.body.firstName,
+    surname: req.body.surname,
+    email: req.body.email,
+    roomId: req.body.roomId,
+    checkInDate: req.body.checkInDate,
+    checkOutDate: req.body.checkOutDate
+  }
+  bookings.push(newBooking);
+  res.json(bookings);
+})
+
+
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
