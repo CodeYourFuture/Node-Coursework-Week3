5c145ef61a4800ad4ea213d69c627805e1e75161
justsealan <justsealan@gmail.com>
2022-04-29 13:01:36 +0100

    Add create new booking

--- server.js
+++ server.js
@@ -9,12 +9,40 @@ app.use(cors());
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+app.get("/", function (req, res) {
+  res.send("Hotel booking server.  Ask for /bookings, etc.");
+});
+
+// Create new booking
+app.post("/bookings", function (req, res) {
+  const newBooking = {
+    id: bookings.length + 1,
+    title: req.body.title,
+    name: req.body.name,
+    surname: req.body.surname,
+    email: req.body.email,
+    roomId: req.body.roomId,
+    checkInDate: req.body.checkInDate,
+    checkOutDate: req.body.checkOutDate,
+  };
+  if (
+    !newBooking.title ||
+    !newBooking.name ||
+    !newBooking.surname ||
+    !newBooking.email ||
+    !newBooking.roomId ||
+    !newBooking.checkInDate ||
+    !newBooking.checkOutDate
+  ) {
+    res.status(400).send("Invalid data");
+  }
+  bookings.push(newBooking);
+
+  res.send(bookings);
 });
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 5001, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
