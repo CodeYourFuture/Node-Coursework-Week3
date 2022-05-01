7c7eeca6c44f144290e0d7957a032cd340d8a95f
Alirezabg <91377914+Alirezabg@users.noreply.github.com>
2022-05-01 12:44:23 +0100

    level 1

--- server.js
+++ server.js
@@ -10,11 +10,14 @@ app.use(express.static("public"));
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
+const bookingsDefault = require("./bookings-copy.json")
 const { response } = require("express");
 const { del } = require("express/lib/application");
 
 app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+  response.send(
+    "Hotel booking server.  Ask for /bookings, etc.<br>/bookings/:id <br> /bookings/delete/:id <br>  /newbooking/:id/:roomId/:title/:firstName/:surName/:email/:checkInDate/:checkOutDate <br>  <br> "
+  );
 });
 app.get("/bookings", (req, res) => {
   res.send(bookings);
@@ -28,10 +31,27 @@ app.get("/bookings/delete/:id", (req, res) => {
   const index = bookings.findIndex((object) => {
     return object.id === delId;
   });
-bookings.splice(index, 1);
+  bookings.splice(index, 1);
   const json = JSON.stringify(bookings, null, 2);
   // 3
   fs.writeFile("./bookings.json", json, "utf-8", (error) => {
+    // 4
+    if (error) {
+      console.log(`WRITE ERROR: ${error}`);
+    } else {
+      // 5
+      console.log("FILE WRITTEN TO");
+    }
+  });
+
+  res.send(bookings);
+});
+app.get("/bookings/default", (req, res) => {
+  
+  
+  const json = JSON.stringify(bookingsDefault, null, 2);
+  // 3
+  fs.writeFile("./bookings.json", "./bookings-copy.json", "utf-8", (error) => {
     // 4
     if (error) {
       console.log(`WRITE ERROR: ${error}`);
