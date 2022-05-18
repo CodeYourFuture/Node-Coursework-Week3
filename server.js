f21ae6e5e7e9c9d98c29241b9a3eda889ed90717
Hatice06 <haticeaydogan00@gmail.com>
2022-05-18 21:33:00 +0100

    Create post request

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -9,11 +10,26 @@ app.use(cors());
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+app.get("/", function (req, res) {
+  res.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
 // TODO add your routes and helper functions here
+
+//Create new booking
+
+app.post("/booking", (req, res) => {
+  let { title, firstName, surname, email, roomId, checkInDate, checkOutDate } =
+    req.body;
+
+   bookings.push(req.body);
+   res.send({booking:"Booking is recorded successfully!"})
+});
+
+//Read all bookings
+app.get("/bookings", (req, res) => {
+  res.send(bookings);
+})
 
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
