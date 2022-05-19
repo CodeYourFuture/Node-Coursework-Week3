06fee58939a62a613564847ae173b473aa3af00f
mahri09 <63245237+mahri09@users.noreply.github.com>
2022-05-19 22:34:03 +0100

    created routes for bookings path and created CRUD

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const bookings = require("./routes/bookings");
 
 const app = express();
 
+const port = process.env.PORT || 8090;
+
 app.use(express.json());
 app.use(cors());
-
-//Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
 // TODO add your routes and helper functions here
+app.use("/bookings", bookings);
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+app.listen(port, function () {
+  console.log("Your app is listening on port " + port);
 });
