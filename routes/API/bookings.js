498e7ee1ca91e3623d0e693578fc9627d785f32d
lalehkoupa <lalehkoupa@gmail.com>
2022-05-19 14:10:02 +0100

    London Class8/Laleh Koupaei/Node-Week3
     Up to level 6 completed

---
+++ routes/API/bookings.js
@@ -0,0 +1,114 @@
+const express = require("express");
+
+const router = express.Router();
+const uuid = require("uuid");
+const bookings = require("../../data/Bookings");
+const moment = require("moment");
+const emailValidator = require("email-validator");
+
+//get all booking
+router.get("/", (req, res) => {
+  res.send(bookings);
+});
+
+router.get("/search", (req, res) => {
+  const wordEntered = req.query.term;
+  const dateEntered = req.query.date;
+
+  //search by firstName,surName & email
+  if (wordEntered) {
+    const newBooking = bookings.filter(
+      (booking) =>
+        booking.firstName.toUpperCase().includes(wordEntered.toUpperCase()) ||
+        booking.surname.toUpperCase().includes(wordEntered.toUpperCase()) ||
+        booking.email.toUpperCase().includes(wordEntered.toUpperCase())
+      //(booking.firstName || booking.surname || booking.email)
+      //   .toUpperCase()
+      //   .includes(wordEntered)
+    );
+
+    return res.send(newBooking);
+
+    //search by date
+  } else {
+    if (moment(dateEntered, "YYYY-MM-DD", true).isValid()) {
+      return res.send(
+        bookings.filter((booking) =>
+          moment(dateEntered).isBetween(
+            booking.checkInDate,
+            booking.checkOutDate,
+            null,
+            "[]"
+          )
+        )
+      );
+    } else {
+      return res
+        .status(404)
+        .json({ msg: "please enter the date format correctly YYYY-MM-DD" });
+    }
+  }
+});
+
+//get one booking with a specific id
+router.get("/:id", (req, res) => {
+  res.send(
+    bookings.filter((booking) => booking.id === parseInt(req.params.id))
+  );
+});
+
+//create new booking
+//in Postman we should set header (content-type & application/json)
+router.post("/", (req, res) => {
+  const {
+    title,
+    firstName,
+    surName,
+    email,
+    roomId,
+    checkInDate,
+    checkOutDate,
+  } = req.body;
+  //check all field been filled up
+  if (
+    !title ||
+    !firstName ||
+    !surName ||
+    !email ||
+    !roomId ||
+    !checkInDate ||
+    !checkOutDate
+  ) {
+    return res.status(404).send("please Enter all of the fields");
+    //validate email
+  } else if (!emailValidator.validate(email)) {
+    res.status(404).json({ msg: "please enter the valid email" });
+    //validate check in date is before the che out date
+  } else if (!moment(checkOutDate).isAfter(checkInDate)) {
+    res
+      .status(404)
+      .json({ msg: "Check out date should be greater than the check in date" });
+  }
+  const newBooking = {
+    id: uuid.v4(),
+    title,
+    firstName,
+    surName,
+    email,
+    roomId,
+    checkInDate,
+    checkOutDate,
+  };
+  bookings.push(newBooking);
+  res.send(bookings);
+});
+
+//Delete booking
+
+router.delete("/:id", (req, res) => {
+  res.send(
+    bookings.filter((booking) => booking.id !== parseInt(req.params.id))
+  );
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 
-const app = express();
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use("/api/bookings", require("./routes/API/bookings"));
 
-//Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
-
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
-});
-
-// TODO add your routes and helper functions here
-
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
