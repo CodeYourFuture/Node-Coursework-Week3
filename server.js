5873af56a7cc66bce72eae22b868db7e73659f20
Marina Almeida <67846587+MarinaAlmeida20@users.noreply.github.com>
2022-05-19 14:37:10 +0100

    level 2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const bodyParse = require('body-parser')
 const app = express();
 
 app.use(express.json());
@@ -9,12 +9,88 @@ app.use(cors());
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+const port = 8080;
+
+const booking = bookings;
+
+app.get("/", (req, res) => {
+  res.json("Hotel booking server.  Ask for /bookings, etc.");
 });
 
-// TODO add your routes and helper functions here
+// Read all bookings
+app.get("/bookings", (req, res) => {
+  res.json(booking);
+});
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+// Create a new booking
+app.post("/bookings", (req, res, next) => {
+  const {
+    title,
+    firstName,
+    surname,
+    email,
+    roomId,
+    checkInDate,
+    checkOutDate,
+  } = req.body;
+
+  const newBooking = {
+    id: booking.length + 1,
+    title,
+    firstName,
+    surname,
+    email,
+    roomId,
+    checkInDate,
+    checkOutDate,
+  };
+
+  if (
+    !newBooking.title ||
+    !newBooking.firstName ||
+    !newBooking.surname ||
+    !newBooking.email ||
+    !newBooking.roomId ||
+    !newBooking.checkInDate ||
+    !newBooking.checkOutDate
+  ) {
+    return res.status(400).json("Please include all property of the booking");
+  }
+
+  booking.push(newBooking);
+
+  res.json(booking);
+
+  next()
+});
+
+// Read one booking, specified by an ID
+app.get("/bookings/:id", (req, res, next) => {
+  const foundId = booking.find((i) => i.id === Number(req.params.id));
+
+  if (foundId) {
+    return res.status(200).json(foundId);
+  }
+
+  res.status(404).json("booking not found");
+
+  next()
+});
+
+// Delete a booking, specified by an ID
+app.delete("/bookings/:id", (req, res) => {
+  const foundId = booking.find((i) => i.id === Number(req.params.id));
+
+  if (foundId) {
+    return res.status(200).json({
+      msg: `Booking id: ${req.params.id} deleted `,
+      "All bookings: ": booking.filter((i) => i.id !== Number(req.params.id)),
+    });
+  }
+
+  res.status(404).json("booking not found");
+});
+
+app.listen(port, () => {
+  console.log(`Listen in http://localhost:${port}`);
 });
