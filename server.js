fa5899d729a97d13e6d05558439c21a76e7cb61a
Timea <timea.reich@gmail.com>
2022-05-21 00:35:39 +0100

    level 2-5

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const validator = require("email-validator");
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
+const moment = require("moment");
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
@@ -15,6 +17,35 @@ app.get("/", function (request, response) {
 
 app.get("/bookings", (req, res) => {
   res.json(bookings);
+});
+
+app.get("/bookings/search", (req, res) => {
+  console.log(req.query.date);
+
+  if (req.query.date !== undefined) {
+    const dateToFind = moment(req.query.date);
+    console.log(dateToFind);
+    const matchingDates = bookings.filter(
+      (item) =>
+        moment(item.checkInDate) <= dateToFind &&
+        moment(item.checkOutDate) >= dateToFind
+    );
+    res.json(matchingDates);
+  }
+  if (req.query.term !== undefined) {
+    const searchTerm = req.query.term.toUpperCase();
+    console.log(searchTerm);
+    const matchingTerms = bookings.filter((item) => {
+      return (
+        item.email.toUpperCase().includes(searchTerm) ||
+        item.firstName.toUpperCase().includes(searchTerm) ||
+        item.surname.toUpperCase().includes(searchTerm)
+      );
+    });
+    res.json(matchingTerms);
+  }
+  //  moment(req.query.time);
+  //const date = new Date("2019-05-06");
 });
 
 app.get("/bookings/:id", (req, res) => {
@@ -49,12 +80,22 @@ app.post("/bookings", (req, res) => {
     newBooking.checkOutDate === undefined
   ) {
     res.json("Please fill all fields.");
+  }
+  if (!validator.validate(newBooking.email)) {
+    res.status(400).json({ message: "Please enter valid email address." });
+  }
+  if (
+    (moment(newBooking.checkInDate).diff(newBooking.checkOutDate), "day" < 0)
+  ) {
+    res
+      .status(400)
+      .json({ message: "Please enter valid check in and checkout dates." });
   } else {
     bookings.push(newBooking);
     res.json("Booking Added");
   }
 });
-
+console.log();
 //////////does not modify the data just filters it//////
 
 // app.delete("/bookings:id", (req, res) => {
