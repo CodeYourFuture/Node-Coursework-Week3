a6cd4a177d925851f3e4df281766443fbfb1ed3d
craig-dsilva <90406619+craig-dsilva@users.noreply.github.com>
2022-05-20 16:50:24 +0100

    change port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
