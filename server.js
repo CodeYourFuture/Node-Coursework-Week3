9917b67eb3ce52f89c97601d58325fce9572a710
Margarita Glushkova <m.glushkova1@gmail.com>
2022-06-25 10:31:09 +0100

    package-lock added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const moment = require("moment");
 const validator = require("email-validator");
+const { Pool } = require("pg");
 
 const app = express();
 const m = moment();
@@ -9,8 +10,17 @@ app.use(express.json());
 app.use(cors());
 
 //Use this array as your (in-memory) data store.
-let bookings = require("./bookings.json");
+//let bookings = require("./bookings.json");
+const pool = new Pool({
+  connectionString: process.env.PORT,
+  user: "postgres",
+  host: "localhost",
+  database: "cyf_hotels",
+  password: "",
+  port: 5432,
+});
 const { json } = require("express");
+const res = require("express/lib/response");
 
 app.get("/", (req, res) => {
   res.send("Hotel booking server.  Ask for /bookings, etc.");
@@ -19,7 +29,13 @@ app.get("/", (req, res) => {
 // TODO add your routes and helper functions here
 
 app.get("/bookings", (req, res) => {
-  res.json(bookings);
+  pool
+    .query("SELECT * FROM bookings")
+    .then((result) => res.json(result.rows))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 app.get("/bookings/search", (req, res) => {
@@ -52,85 +68,131 @@ app.get("/bookings/search", (req, res) => {
 
 app.get("/bookings/:id", (req, res) => {
   const requestedID = Number(req.params.id);
-  const found = bookings.some((booking) => booking.id === requestedID);
-  if (found) {
-    res.json(bookings.filter((booking) => booking.id === requestedID));
-  } else {
-    res.status(404).json({ msg: `Booking with id ${requestedID} not found.` });
-  }
+  pool
+    .query(`SELECT * FROM bookings WHERE id = ${requestedID}`)
+    .then((result) => {
+      if (result.rowCount === 0)
+        return res
+          .status(404)
+          .json({ msg: `Booking with id ${requestedID} not found.` });
+      else return res.json(result.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 app.delete("/bookings/:id", (req, res) => {
   const requestedID = Number(req.params.id);
-  const found = bookings.some((booking) => booking.id === requestedID);
-  if (found) {
-    const deletedMessage = bookings.filter(
-      (booking) => booking.id === requestedID
-    );
-    const { title, firstName, surname, id } = deletedMessage[0];
-    bookings = bookings.filter((booking) => booking.id !== requestedID);
-    res.json({
-      msg: `Booking with id ${id} for ${title} ${firstName} ${surname} has now been deleted`,
-      bookings,
-      deletedMessage,
+  pool
+    .query(`DELETE FROM bookings WHERE id = $1`, [requestedID])
+    .then((result) => {
+      result.rowCount;
+      if (result.rowCount === 0)
+        return res
+          .status(404)
+          .json({ msg: `Booking with id ${requestedID} not found.` });
+      else return res.json(result.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-  } else {
-    res.status(404).json({ msg: `Booking with id ${requestedID} not found.` });
+});
+
+// app.delete("/customers/:id/bookings", (res, req))
+// //customers/:id/bookings
+
+app.post("/hotels", async (req, res) => {
+  const { hotelName, hotelRooms, hotelPostcode } = req.body;
+  const newHotel = {
+    hotelName,
+    hotelRooms: Number(hotelRooms),
+    hotelPostcode,
+  };
+  try {
+    const alreadyExist = await pool.query(
+      "SELECT * FROM hotels WHERE name = $1",
+      [hotelName]
+    );
+    console.log(alreadyExist);
+    if (alreadyExist.rowCount)
+      return res.status(400).json("A hotel with the same name already exist");
+    else {
+      const roomsValidator = hotelRooms && Number.isInteger(hotelRooms);
+      const hotelNameValidator = hotelName && hotelName.length <= 120;
+      const errorMessage = {};
+      if (!roomsValidator)
+        errorMessage = { msg: "incorrect input for number of rooms" };
+      if (!hotelNameValidator)
+        errorMessage = { msg: "incorrect input for hotel name" };
+      if (Object.keys(errorMessage).length) {
+        return res.status(400).json(errorMessage);
+      }
+      console.log(newHotel);
+      try {
+        const insertedRowHotels = await pool.query(
+          `INSERT INTO hotels (name, rooms, postcode) VALUES ($1, $2, $3) RETURNING id`,
+          [newHotel.hotelName, newHotel.hotelRooms, newHotel.hotelPostcode]
+        );
+
+        return res.json({
+          msg: `You have submitted new booking with id ${insertedRowHotels.rows[0].id}.`,
+        });
+      } catch (e) {
+        console.log(e);
+        res.status(400).json("Something went wrong");
+      }
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(400).json("Something went wrong");
   }
 });
 
-app.post("/bookings", (req, res) => {
-  const {
-    title,
-    firstName,
-    surname,
-    email,
-    roomId,
-    checkInDate,
-    checkOutDate,
-  } = req.body;
+app.post("/bookings", async (req, res) => {
+  const { customerId, hotelId, checkInDate, nights } = req.body;
   const newBooking = {
-    id: bookings.length + 1,
-    title: title.charAt(0).toUpperCase() + title.slice(1),
-    firstName: firstName.charAt(0).toUpperCase() + firstName.slice(1),
-    surname: surname.charAt(0).toUpperCase() + surname.slice(1),
-    email,
-    roomId: Number(roomId),
-    checkInDate,
-    checkOutDate,
+    customerId: Number(customerId),
+    hotelId: Number(hotelId),
+    checkInDate: checkInDate,
+    nights: Number(nights),
   };
-  const checkInDateAfterCheckOut = moment(checkInDate).isAfter(checkOutDate);
-  const isEmailValid = validator.validate(email);
-  const isEmptyKey = Object.values(newBooking).some(
-    (x) => x === null || x === ""
-  );
-  const compulsoryFields = Object.keys(newBooking).slice(1);
+  const dateValidator = new Date(newBooking.checkInDate).getTime() > 0;
+  const compulsoryFields = Object.keys(newBooking);
   const allKeysArePresent = compulsoryFields.every((key) => {
     return Object.keys(req.body).includes(key);
   });
   const errorMessage = {};
-  if (
-    isEmptyKey ||
-    !allKeysArePresent ||
-    !isEmailValid ||
-    checkInDateAfterCheckOut
-  ) {
-    if (isEmptyKey || !allKeysArePresent) {
-      errorMessage.msgMissingKey = "Some information is missing";
-    }
-    if (!isEmailValid) {
-      errorMessage.msgInvalidEmail = "Invalid email format";
-    }
-    if (checkInDateAfterCheckOut) {
-      errorMessage.wrongCheckInDate = "Check-in can not be after Check-out";
-    }
-    res.status(400).json(errorMessage);
-  } else {
-    bookings.push(newBooking);
+  if (!allKeysArePresent) {
+    errorMessage.msgMissingKey = "Some information is missing";
+  }
+  if (!dateValidator) {
+    console.log(newBooking.checkInDate);
+    errorMessage.msgInvalidDate = "Invalid date";
+  }
+  if (Object.keys(errorMessage).length) {
+    return res.status(400).json(errorMessage);
+  }
+
+  try {
+    const insertedRow = await pool.query(
+      `INSERT INTO bookings (customer_id, hotel_id, checkin_date, nights) VALUES ($1, $2, $3, $4) RETURNING id`,
+      [
+        newBooking.customerId,
+        newBooking.hotelId,
+        newBooking.checkInDate,
+        newBooking.nights,
+      ]
+    );
+    console.log(insertedRow);
     return res.json({
-      msg: `You have submitted new booking with id ${newBooking.id} for ${title} ${firstName} ${surname}.`,
-      bookings,
+      msg: `You have submitted new booking with id ${insertedRow.rows[0].id}.`,
     });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json("Something went wrong");
   }
 });
 // const listener = app.listen(process.env.PORT, function () {
