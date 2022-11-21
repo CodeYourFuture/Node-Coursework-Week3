d69137b7f997a000b0dbd9ecd58b7e17bc4c18e6
Anthony Mogotlane <almogotlane@gmail.com>
2022-11-21 18:08:42 +0200

    Added get, post and delete methods to server.js
    The api can allow a client to create a new booking, read all bookings, read one booking specified by id and delete one booking with a specified id

--- server.js
+++ server.js
@@ -7,14 +7,50 @@ app.use(express.json());
 app.use(cors());
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+let bookings = require("./bookings.json");
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
-// TODO add your routes and helper functions here
+app.post("/bookings", (req, res) => {
 
-const listener = app.listen(process.env.PORT, function () {
+  const booking = {
+    "id": bookings.length,
+    "title": req.body.title,
+    "firstName": req.body.firstName,
+    "surname": req.body.surname,
+    "email": req.body.email,
+    "roomId": req.body.roomId,
+    "checkInDate": req.body.checkInDate,
+    "checkOutDate": req.body.checkOutDate
+  };
+
+  res.status(200).json(booking);
+})
+
+app.get("/bookings", (req, res) => {
+  res.json(bookings);
+})
+
+app.get("/bookings/:id", (req, res) => {
+  if (bookings.some(b => b.id === req.params.id - "")) {
+    res.status(200).json(bookings.find(b => b.id === req.params.id - ""));
+  } else {
+    res.status(404).json({ message: "unsuccessful" });
+  }
+})
+
+app.delete("/bookings/:id", (req, res) => {
+  if (bookings.some(b => b.id === req.params.id - "")) {
+    bookings = bookings.filter(b => b.id !== req.params.id - "");
+    res.status(200).json(bookings);
+  } else {
+    res.status(404).json({ message: "unsuccessful" });
+  }
+})
+
+// TODO add your routes and helper functions here
+const listener = app.listen(process.env.PORT || 3001, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
