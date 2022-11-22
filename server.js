c60387729c5c83182d98708f86a2e927b0317685
Riyaaz Singh <riyaazct1@gmail.com>
2022-11-22 20:49:39 +0200

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const port = process.env.PORT || 3001;
 
 const app = express();
 
@@ -13,8 +14,53 @@ app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
-// TODO add your routes and helper functions here
+// Read all bookings
+app.get("/bookings", (req, res) => res.json(bookings));
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+//Add a new booking
+app.post("/bookings", (req, res) => {
+  let newBooking = {
+    id: bookings.length + 1,
+    roomId: req.body.roomId,
+    title: req.body.title,
+    firstName: req.body.firstName,
+    surname: req.body.surname,
+    email: req.body.email,
+    checkInDate: req.body.checkInDate,
+    checkOutDate: req.body.checkInDate,
+  };
+  if (
+    !newBooking.roomId ||
+    !newBooking.title ||
+    !newBooking.firstName ||
+    !newBooking.surname ||
+    !newBooking.email ||
+    !newBooking.checkInDate ||
+    !newBooking.checkOutDate
+  ) {
+    res
+      .status(400)
+      .json({ msg: "Please fill in all required fields" });
+  }
+  bookings.push(newBooking);
+  res.send(bookings);
+}); // End of add a booking
+
+// Read booking by ID
+app.get("/bookings/:id", (req, res) => {
+  let bookingId = parseInt(req.params.id);
+  let found = bookings.some((booking) => booking.id === bookingId);
+  if (found) {
+    res.json(bookings.find((booking) => booking.id === bookingId));
+  } else {
+    res.status(404).json({
+      msg: `No booking with id ${bookingId} found`,
+    });
+  }
+});
+
+const listener = app.listen(port, function () {
+  console.log(
+    "Your app is listening on port " + listener.address().port
+  );
 });
