6d95eb0cfa30795f545bcb91f79dada34049fa75
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-09 22:07:45 +0000

    added body parser

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const validator = require("email-validator");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 //Use this array as your (in-memory) data store.
