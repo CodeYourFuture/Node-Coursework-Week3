c7e38afbd5e9554ca6330226562ff34b2b35fbac
vanipriya_piramanathan <vpriyaarun23@gmail.com>
2023-01-10 22:33:35 +0000

    level-5 completed

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const validator = require('validator');
-const moment = require('moment');
-
+const validator = require("validator");
+const moment = require("moment");
 
 const app = express();
 
@@ -21,75 +20,40 @@ app.get("/", function (request, response) {
 
 //Read all messages
 app.get("/bookings", (req, res) => {
-  res.send(bookings);
+  res.json(bookings);
 });
 
 //Read all messages by search term
 app.get("/bookings/search", (req, res) => {
-  const term = req.query.term;
-  const date = req.query.date;
-
-  if (term) {
+  if (req.query.term) {
     // search by term
-    const searchResults = bookings.filter(booking => {
+    const term = req.query.term.toLowerCase();
+    const searchResults = bookings.filter((booking) => {
       return (
-        booking.firstName.toLowerCase().includes(term.toLowerCase()) ||
-        booking.surname.toLowerCase().includes(term.toLowerCase()) ||
-        booking.email.toLowerCase().includes(term.toLowerCase())
-      )
-    })
-    res.send(searchResults)
-  } else if (date) {
+        booking.firstName.toLowerCase().includes(term) ||
+        booking.surname.toLowerCase().includes(term) ||
+        booking.email.toLowerCase().includes(term)
+      );
+    });
+    res.json(
+      searchResults.length === 0 ? { msg: `Booking not found` } : searchResults
+    );
+  } else if (req.query.date) {
     // search by date
-    const searchResults = bookings.filter(booking => {
+    const date = moment(req.query.date);
+    const searchResults = bookings.filter((booking) => {
       return (
-        booking.checkInDate <= date && booking.checkOutDate >= date
-      )
-    })
-    res.send(searchResults)
+        moment(booking.checkInDate) <= date &&
+        moment(booking.checkOutDate) >= date
+      );
+    });
+    res.json(
+      searchResults.length === 0 ? { msg: `Booking not found` } : searchResults
+    );
   } else {
-    res.send('Please provide a search term or date')
+    res.send("Please provide a search term or date");
   }
-  // const term = req.query.term ? req.query.term.toLowerCase() :"";
-  // const date=req.query.date ? moment(req.query.date): "";
-  // console.log(`item is${term}`);
-  // console.log(date)
-  // console.log(moment(bookings[0].checkInDate));
-  // // if (!req.body.term && !req.body.date) {
-  // //   res.status(400).send("Please add search item");
-  // //   return;
-  // // }
-  // console.log("date",moment(bookings[0].checkInDate).isSameOrBefore(date) && moment(bookings[0].checkOutDate).isSameOrAfter(date));
-  // console.log("date",moment(bookings[0].checkInDate).isSame(date));
-  
-  // // Search for bookings that match the search term and span the given date
-
-  // let searchedBooking = bookings.filter((c) =>
-  //  c.firstName.toLowerCase().includes(term) ||c.surname.toLowerCase().includes(term) || c.email.includes(term) || moment(c.checkInDate).isSame(date)
-  // );
-  // if (searchedBooking.length === 0) {
-  //   res.status(404).json({ msg: `Booking not found` });
-  //   return;
-  // }
-  // res.json(searchedBooking);
 });
-
-//Read all messages by search term
-// app.get("/bookings/search", (req, res) => {
-//   const date=parseInt(req.query.data);
-//   if (!date) {
-//     res.status(400).send("Please add search date");
-//     return;
-//   }
-//   let searchedBooking = bookings.filter((c) =>
-//     c.checkInDate.includes(date)
-//   );
-//   if (searchedBooking.length === 0) {
-//     res.status(404).json({ msg: `Booking not found` });
-//     return;
-//   }
-//   res.json(searchedBooking);
-// });
 
 //Read one Booking specified by an ID
 app.get("/messages/:id", (req, res) => {
@@ -103,35 +67,66 @@ app.get("/messages/:id", (req, res) => {
   res.json(booking);
 });
 
-// Create a new booking
+// Create a new booking (POST)
 app.use(express.json());
-const compulsoryFields = ['title', 'firstName', 'surname', 'email','roomId','checkInDate','checkOutDate'];
-app.post("/booking", (req, res) => {
+const compulsoryFields = [
+  "title",
+  "firstName",
+  "surname",
+  "email",
+  "roomId",
+  "checkInDate",
+  "checkOutDate",
+];
+app.post("/bookings", (req, res) => {
   // validate our input
   //1.validate if all the fields are included
-   if (!compulsoryFields.every(cf => req.body.hasOwnProperty(cf))) {
-        res.status(401).send('Not all compulsory fields supplied');
-        return;
-    }
+  if (!compulsoryFields.every((cf) => req.body.hasOwnProperty(cf))) {
+    res.status(401).send("Not all compulsory fields supplied");
+    return;
+  }
   //2.validate the email
-    if (!validator.isEmail(req.body.email)) {
-        res.status(401).send('Email is not valid');
-        return;
-    }
+  if (!validator.isEmail(req.body.email)) {
+    res.status(401).send("Email is not valid");
+    return;
+  }
   //3. validate checkoutDate is after checkinDate and format of date "yyyy-mm-dd"
-    const checkIn = moment(req.body.checkInDate,'YYYY-MM-DD');
-    const checkOut = moment(req.body.checkOutDate,'YYYY-MM-DD');
-    if (!checkIn.isValid() && !checkOut.isValid()) {
-       res.status(401).send("CheckIn/Out date is not valid");
-        return;
+  const checkIn = moment(req.body.checkInDate, "YYYY-MM-DD");
+  const checkOut = moment(req.body.checkOutDate, "YYYY-MM-DD");
+  if (!checkIn.isValid() && !checkOut.isValid()) {
+    res.status(401).send("CheckIn/Out date is not valid");
+    return;
+  }
+  if (!checkOut.isAfter(checkIn)) {
+    res.status(401).send("CheckOut date can't be before Checkin date ");
+    return;
+  }
+  const newEntry = { id: ++maxID };
+  compulsoryFields.forEach((fld) => {
+    if (req.body[fld]) {
+      newEntry[fld] = req.body[fld];
     }
-    if (!checkOut.isAfter(checkIn)) {
-        res.status(401).send("CheckOut date can't be before Checkin date ");
-        return;
-    }
-    const newEntry = { id: ++maxID };
-    compulsoryFields.forEach(fld => { if (req.body[fld]) { newEntry[fld] = req.body[fld] } });
-    bookings.push(newEntry);
-    res.send(newEntry)
+  });
+  bookings.push(newEntry);
+  res.json(newEntry);
 });
 
+// Delete Booking from bookings data(DELETE)
+app.delete("/bookings/:id", function (req, res) {
+  const bookingId = parseInt(req.params.id);
+  if (isNaN(bookingId)) {
+    res.sendStatus(400);
+    return;
+  }
+  const booking = bookings.find((a) => a.id === bookingId);
+  if (!booking) {
+    res.status(404).send("Booking is not found");
+    return;
+  }
+  const index = bookings.findIndex((a) => a.id === bookingId);
+  bookings.splice(index, 1);
+  res.json({ msg: "booking deleted", bookings });
+});
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
