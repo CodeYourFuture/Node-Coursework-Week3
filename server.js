f5c6c3ae03d6166a9e248551a06da1d24d01e381
Abdirahim Hussein <drahimhussein@gmail.com>
2023-01-11 23:33:49 +0000

    completed 1. Create a new booking Read all bookings

--- server.js
+++ server.js
@@ -12,6 +12,38 @@ const bookings = require("./bookings.json");
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
+// gets all bookings 
+app.get("/bookings", (request, response) => {
+res.send(bookings);
+});
+
+//Creates a new booking for data
+app.post("/bookings", (request, response) => {
+  let newBookingTable = {
+    id: 0,
+    title: request.body.title,
+    firstName: request.body.firstName,
+    surname: request.body.surname,
+    email: request.body.email,
+    roomId: 0,
+    checkInDate: request.body. checkInDate,
+    checkOutDate: request.body.checkOutDate,
+  }
+});
+let  {title, firstName, surname, email, checkInDate, checkOutDate} = request.body;
+  if(
+    !title ||
+    !surname ||
+    !email ||
+    !checkInDate ||
+    !checkOutDate 
+  ){
+  
+    response.status(400).send({message: " ERROR All fields have not been completed please try again"});
+  }
+  bookings.push(newBookingTable);
+  newBookingTable.id = bookings.findIndex(newBooking) + 1;
+  newBookingTable.roomId = newBookingTable.id + 10;
 
 // TODO add your routes and helper functions here
 
