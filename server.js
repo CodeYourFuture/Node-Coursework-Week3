f25c1827c56ecd18f1d7a9bb58214c191612d688
jaspreet kaur <kaurjas1928@yahoo.com>
2023-01-12 19:33:04 +0000

    updated API for deletion

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 //Read all bookings
-app.get("/bookings", function(request,response){
+app.get("/bookings", function (request, response) {
   response.send(bookings);
-})
+});
 
 //Create new bookings
 app.post("/bookings", (req, res) => {
@@ -56,16 +56,28 @@ app.post("/bookings", (req, res) => {
 });
 
 // Read one booking, specified by an ID
-app.get("/bookings/:id", (req,res) => {
+app.get("/bookings/:id", (req, res) => {
   let id = parseInt(req.params.id);
-  let requiredBooking = bookings.find(reqId => reqId.id === id)
-  if(!requiredBooking){
+  let requiredBooking = bookings.find((reqId) => reqId.id === id);
+  if (!requiredBooking) {
     res.status(404).send("Please type a valid ID");
-  } else{
-  res.send(requiredBooking);
+  } else {
+    res.send(requiredBooking);
   }
-})
+});
 
+//Delete a booking, specified by an ID
+app.delete("/bookings/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  const notDeleted = bookings.filter((booking) => booking.id !== id);
+  let toDelete = bookings.find((booking) => booking.id == id);
+  if (toDelete === undefined) {
+    res.status(404).send("Please type a valid ID");
+  } else {
+    res.status(200).json({ success: true });
+  }
+  bookings = notDeleted;
+});
 // TODO add your routes and helper functions here
 
 const listener = app.listen(process.env.PORT, function () {
