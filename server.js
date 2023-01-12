04684a0aefa306efe01669e020fb8ed12b03966c
Chidimma <chidimmamofodum@gmail.com>
2023-01-12 16:44:30 +0000

    including validations for requests

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const moment = require("moment");
+const validator = require("email-validator");
 
 const app = express();
 
@@ -27,8 +29,11 @@ function getMaxId(arr) {
 }
 
 function isValid(obj) {
-  let receivedData = Object.keys(obj);
-  return Model.every((x) => receivedData.includes(x));
+  const receivedData = Object.keys(obj);
+  if (Model.every((x) => receivedData.includes(x))) {
+    return validator.validate(obj.email);
+  }
+  return false;
 }
 
 //.....................Routes................................................
@@ -37,13 +42,24 @@ app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
-
 // 1. Read all bookings
 app.get("/bookings", (req, res) => {
   res.json(bookings);
 });
 
-// 1. Read one booking, specified by an ID
+app.get("/bookings/search", (req, res) => {
+  if (req.query.date) {
+    const inputDate = moment(req.query.date);
+    const result = bookings.filter((item) =>
+      moment(item.checkInDate).isSame(moment(inputDate))
+    );
+    res.json(result);
+  } else {
+    res.status(400).send("Bad request");
+  }
+});
+
+
 app.get("/bookings/:id", (req, res) => {
   const id = req.params.id;
   const result = bookings.find((item) => item.id === +id);
@@ -54,7 +70,7 @@ app.get("/bookings/:id", (req, res) => {
   }
 });
 
-// 1. Create a new booking
+
 app.post("/bookings", (req, res) => {
   const result = {};
   const id = getMaxId(bookings) + 1;
@@ -65,31 +81,23 @@ app.post("/bookings", (req, res) => {
     }
     bookings.push(result);
     res.json(bookings);
-
   } else {
-    res.status(400).send("Please provide all mandatory data");
+    res.status(400).send("Please provide all mandatory data or a valid email");
   }
 });
 
-// 1. Delete a booking, specified by an ID
+
 app.delete("/bookings/:id", (req, res) => {
   let id = req.params.id;
   const id_data = bookings.find((item) => item.id === +id);
-  if(id_data) {
-     const result = bookings.filter((item) => item.id !== +id);
-      res.json(result);
+  if (id_data) {
+    const result = bookings.filter((item) => item.id !== +id);
+    res.json(result);
   } else {
-    res.status(404).send("Not found")
+    res.status(404).send("Not found");
   }
 });
 
-// If the booking to be read cannot be found by id, return a 404.
-
-// If the booking for deletion cannot be found by id, return a 404.
-
-// All booking content should be passed as JSON.
-
-// See the later spoiler section "Correct Routes" if you are not sure of the correct routes.
 
 const listener = app.listen(process.env.PORT || 5000, function () {
   console.log("Your app is listening on port " + 5000);
