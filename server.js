fd9afc23903ca47c4d92ae6f97e902a51d41a574
Abdirahim Hussein <drahimhussein@gmail.com>
2023-01-17 21:34:51 +0000

    code indented to make it easier to read

--- server.js
+++ server.js
@@ -11,33 +11,36 @@ app.use(cors());
 const bookings = require("./bookings.json");
 
 app.get("/", function (request, response) {
-response.send("Hotel booking server.  Ask for /bookings, etc.");
+  response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
-// gets all bookings 
+// gets all bookings
 app.get("/bookings", (req, res) => {
-res.send(bookings);
+  res.send(bookings);
 });
 
 app.post("/bookings", (req, res) => {
-const { title, firstName, surname, email, checkInDate, checkOutDate} = req.body;
-if (!title || !surname || !email || !checkInDate || !checkOutDate){
-res.status(400).send({message: " ERROR All fields have not been completed please try again"});
-}
-let newBookingTable = {
-id: 0,
-title: req.body.title,
-firstName: req.body.firstName,
-surname: req.body.surname,
-email: req.body.email,
-roomId: 0,
-checkInDate: req.body.checkInDate,
-checkOutDate: req.body.checkOutDate,
-}
-bookings.push(newBookingTable);
-newBookingTable.id = bookings.findIndex(newBooking) + 1;
-newBookingTable.roomId = newBookingTable.id + 10;
-res.send(newBookingTable);
-res.json(bookings);
+  const { title, firstName, surname, email, checkInDate, checkOutDate } =
+    req.body;
+  if (!title || !surname || !email || !checkInDate || !checkOutDate) {
+    res.status(400).send({
+      message: " ERROR All fields have not been completed please try again",
+    });
+  }
+  let newBookingTable = {
+    id: 0,
+    title: req.body.title,
+    firstName: req.body.firstName,
+    surname: req.body.surname,
+    email: req.body.email,
+    roomId: 0,
+    checkInDate: req.body.checkInDate,
+    checkOutDate: req.body.checkOutDate,
+  };
+  bookings.push(newBookingTable);
+  newBookingTable.id = bookings.findIndex(newBooking) + 1;
+  newBookingTable.roomId = newBookingTable.id + 10;
+  res.send(newBookingTable);
+  res.json(bookings);
 });
 // defines IsInvalidId function
 function isInvalidId(id, bookingIndex, res) {
@@ -53,7 +56,9 @@ function isInvalidId(id, bookingIndex, res) {
 }
 // Get one booking by id
 app.get("/bookings/:id", function (request, response) {
-  let bookingIndex = bookings.findIndex((newBookingTable) => newBookingTable.id == request.params.id);
+  let bookingIndex = bookings.findIndex(
+    (newBookingTable) => newBookingTable.id == request.params.id
+  );
   isInvalidId(request.params.id, bookingIndex, response);
   if (bookingIndex >= 0) {
     let booking = bookings[bookingIndex];
@@ -62,7 +67,9 @@ app.get("/bookings/:id", function (request, response) {
 });
 // Delete one booking by id
 app.delete("/bookings/:id", function (request, response) {
-  let bookingIndex = bookings.findIndex((newBookingTable) => newBookingTable.id == req.params.id);
+  let bookingIndex = bookings.findIndex(
+    (newBookingTable) => newBookingTable.id == req.params.id
+  );
   if (isInvalidId(request.params.id, bookingIndex, response)) return;
   bookings.splice(bookingIndex, 1);
   response.send({ message: "Booking deleted" });
@@ -71,5 +78,5 @@ app.delete("/bookings/:id", function (request, response) {
 // TODO add your routes and helper functions here
 
 const listener = app.listen(process.env.PORT, function () {
-console.log("Your app is listening on port " + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
