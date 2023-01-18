e5a6a774f06808fef07709f0ec7af52d86e5f079
4maziar <53979524+8maziar@users.noreply.github.com>
2023-01-18 00:28:06 +0000

    level 1 complete

--- server.js
+++ server.js
@@ -13,7 +13,38 @@ app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
-// TODO add your routes and helper functions here
+app.get("/bookings", function (req, res) {
+  res.send(bookings);
+});
+
+app.get("/bookings/:id", function (req, res) {
+  const bookingId = +req.params.id;
+  const valid = bookings.some((booking) => booking.id === bookingId);
+  if (valid) {
+    const matched = bookings.find((booking) => booking.id === bookingId);
+    res.send(matched);
+  } else {
+    res.status(404).send("incorrect id");
+  }
+});
+
+app.post("/bookings", function (req, res) {
+  const newBooking = req.body;
+  newBooking.id = bookings.length + 1;
+  bookings.push(newBooking);
+  res.send(bookings);
+});
+
+app.delete("/bookings/:id", (req, res) => {
+  const bookingId = +req.params.id;
+  const valid = bookings.some((booking) => booking.id === bookingId);
+  if (valid) {
+    const filtered = bookings.filter((booking) => booking.id !== bookingId);
+    res.send(filtered);
+  } else {
+    res.status(404).send("incorrect id");
+  }
+});
 
 const listener = app.listen(5000 || process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
