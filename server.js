ce073825772966d293e72d11af0c1075db1598b9
4maziar <53979524+8maziar@users.noreply.github.com>
2023-01-18 02:39:04 +0000

    level 1 & 2 refactored

--- server.js
+++ server.js
@@ -18,14 +18,9 @@ app.get("/bookings", function (req, res) {
 });
 
 app.get("/bookings/:id", function (req, res) {
-  const bookingId = +req.params.id;
-  const valid = bookings.some((booking) => booking.id === bookingId);
-  if (valid) {
-    const matched = bookings.find((booking) => booking.id === bookingId);
-    res.send(matched);
-  } else {
-    res.status(404).send("incorrect id");
-  }
+  const matched = bookings.find((booking) => booking.id === +req.params.id);
+  if (!matched) return res.status(404).send("incorrect id");
+  res.send(matched);
 });
 
 app.post("/bookings", function (req, res) {
@@ -33,23 +28,17 @@ app.post("/bookings", function (req, res) {
   newBooking.id = bookings.length + 1;
   const { title, firstName, surname, email, roomId, checkInDate, checkOutDate } = newBooking;
   const valid = !!title && !!firstName && !!surname && !!email && (roomId === 0 || !!roomId) && !!checkInDate && !!checkOutDate;
-  if (valid) {
-    bookings.push(newBooking);
-    res.send(bookings);
-  } else {
-    res.status(400).send("missing information");
-  }
+  if (!valid) return res.status(400).send("missing information");
+  bookings.push(newBooking);
+  res.send(bookings);
 });
 
 app.delete("/bookings/:id", (req, res) => {
-  const bookingId = +req.params.id;
-  const valid = bookings.some((booking) => booking.id === bookingId);
-  if (valid) {
-    const filtered = bookings.filter((booking) => booking.id !== bookingId);
-    res.send(filtered);
-  } else {
-    res.status(404).send("incorrect id");
-  }
+  const booking = bookings.find((booking) => booking.id === +req.params.id);
+  if (!booking) return res.status(404).send("incorrect id");
+  const index = bookings.indexOf(booking);
+  bookings.splice(index, 1);
+  res.send(bookings);
 });
 
 const listener = app.listen(5000 || process.env.PORT, function () {
