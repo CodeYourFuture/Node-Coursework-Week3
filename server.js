17ca45284e664e3acea5d58c959ed4365486dbf8
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-01-19 22:52:20 +0000

    complete level 1

--- server.js
+++ server.js
@@ -8,13 +8,34 @@ app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
-
 app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+  response.send({ bookings });
 });
+//Read all bookings
 
+app.get("/booking", function (request, response) {
+  response.send({ bookings });
+});
+//Create a new booking
+app.post("/booking", (req, resp) => {
+  const newBooking = req.body;
+  bookings.push(newBooking);
+  resp.send("Create Booking has been added >>");
+});
+//Read one booking, specified by an ID
+app.get("/booking/:id", function (request, response) {
+  const bookingID = +request.params.id;
+  const fliterBooking = bookings.filter((booking) => booking.id === bookingID);
+  response.send({ fliterBooking });
+});
+//Delelte booking
+app.delete("/booking/:id", (req, resp) => {
+  const bookingID = +req.params.id;
+  const fliterBooking = bookings.filter((booking) => booking.id !== bookingID);
+  resp.send({ fliterBooking });
+});
 // TODO add your routes and helper functions here
-
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+//process.env.PORT
+const listener = app.listen(9090, function () {
+  console.log("Your app is listening on port " + 9090);
 });
