0a7237d9d8f90da12d98f04a8764585cdde27877
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-01-19 23:25:10 +0000

    Level 2 - simple validation

--- server.js
+++ server.js
@@ -19,19 +19,38 @@ app.get("/booking", function (request, response) {
 //Create a new booking
 app.post("/booking", (req, resp) => {
   const newBooking = req.body;
-  bookings.push(newBooking);
-  resp.send("Create Booking has been added >>");
+  console.log(newBooking);
+  if (
+    newBooking.id &&
+    newBooking.title &&
+    newBooking.firstName &&
+    newBooking.surname &&
+    newBooking.email &&
+    newBooking.roomId &&
+    newBooking.checkInDate &&
+    newBooking.checkOutDate
+  ) {
+    bookings.push(newBooking);
+    resp.send("New Booking has been added >>");
+  } else {
+    resp.send("400");
+  }
 });
 //Read one booking, specified by an ID
 app.get("/booking/:id", function (request, response) {
   const bookingID = +request.params.id;
-  const fliterBooking = bookings.filter((booking) => booking.id === bookingID);
-  response.send({ fliterBooking });
+  const fliterBooking = bookings.find((booking) => booking.id === bookingID);
+  if (!fliterBooking) {
+    response.send("4040");
+  } else {
+    response.send({ fliterBooking });
+  }
 });
 //Delelte booking
 app.delete("/booking/:id", (req, resp) => {
   const bookingID = +req.params.id;
   const fliterBooking = bookings.filter((booking) => booking.id !== bookingID);
+  console.log(fliterBooking);
   resp.send({ fliterBooking });
 });
 // TODO add your routes and helper functions here
