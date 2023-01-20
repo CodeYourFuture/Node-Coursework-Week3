0a00f0979dacac7a79456397fb5287ca790ba2e6
fatih-celebi <celebifatih.c1@gmail.com>
2023-01-20 23:28:27 +0000

    Complete Read & Delete Bookings

--- server.js
+++ server.js
@@ -7,14 +7,34 @@ app.use(express.json());
 app.use(cors());
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+let bookings = require("./bookings.json");
+const { response } = require("express");
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
-// TODO add your routes and helper functions here
+app.get("/bookings", function (req, res) {
+  res.send({bookings})
+})
+
+app.get("/bookings/:id", function (req, res) {
+  const bookingsId = +req.params.id;
+  const oneBooking = bookings.find((booking) => booking.id === bookingsId);
+  res.send(oneBooking);
+})
+
+app.delete("/bookings/:id", function (req, res) {
+  let bookingsId = +req.params.id;
+  bookings = bookings.filter((booking) => booking.id !== bookingsId);
+  res.send({bookings});
+});
 
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+
+
+
+app.listen(9000);
