d8ffe4e66dceb41fb1766a93b59529078d4c7e02
Shima <shimaadnanbasmenj@gmail.com>
2023-01-20 10:47:34 +0000

    level one and two

--- server.js
+++ server.js
@@ -1,20 +1,43 @@
-const express = require("express");
-const cors = require("cors");
+const express=require('express')
+const bodyParser=require('body-parser')
 
-const app = express();
+const app=express()
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+const booking=require('./data/fakeBookings.json')
 
-app.use(express.json());
-app.use(cors());
+app.get('/booking',(req,res)=>{
+    res.json(booking)
+})
+app.get('/booking/:id',(req,res)=>{
+    const inputID=+req.params.id
+    const searchedItembyID=booking.filter(item=>item.id===inputID)
+    res.send(searchedItembyID)
+})
+app.delete('/booking/:id',(req,res)=>{
+    const deletedId=+req.params.id
+    const notDeleteditem=booking.filter(item=>item.id!==deletedId)
+    if(notDeleteditem){res.send(notDeleteditem)}
+    else{res.status(200).send('It is not exist')}
+})
+app.post("/booking",(req,res)=>{
+    const newId=booking[booking.length-1].id+1
+    const {title, firstName,surname,email,roomId,checkInDate,checkOutDate}=req.body
+    const obj={
+      id:newId,
+      ...req.body
+    }
+    console.log(obj);
+    if( !title || !firstName || !surname || !email || !roomId || !checkInDate || !checkOutDate){
+    res.status(400).send("missing value");
+    }else{
+    res.status(201).json({obj})}
+  })
+  app.get("/",(req,res)=>{
+    res.json({booking})
+  })
 
-//Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
-});
-
-// TODO add your routes and helper functions here
-
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+app.listen(4500,(req,res)=>{
+    console.log('The Server is listening 4500')
+})
