098bfe1f5e003b4752dc06cb00c7b163bd24ddb9
ShimenAfshar <afsharshimen@gmail.com>
2023-01-21 11:23:25 +0000

    level 1 done

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const app =expressO();
 const cors = require("cors");
 
 const app = express();
@@ -8,11 +9,26 @@ app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
+const { request, response } = require("express");
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
+app.get("/bookings" , (request,response) => {
+  response.json({ bookings });
+});
+
+app.get("/bookings/:id" , (request ,response) =>{
+  const id = Number(request.params.id);
+  const findId = bookings.find((bookings) => bookings.id ===id);
+  if (!findId) {
+    return response
+    .status(404)
+      .json({ message: `booking with the ${id} not found`});
+  }
+  response.status(200).json({ findId})
+});
 // TODO add your routes and helper functions here
 
 const listener = app.listen(process.env.PORT, function () {
