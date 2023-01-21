271259ef4b73bdddf8c207eb4c85ad012110a792
Ekrem <ekremteke.uk@gmail.com>
2023-01-21 11:52:34 +0000

    created new booking readed all bookings and id

--- server.js
+++ server.js
@@ -8,13 +8,28 @@ app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
+const { request, response } = require("express");
+// reading bookings
+app.get("/bookings", function (request, response) {
+  response.status(20).send({ bookings })
+});
+// create new booking
+app.post("/bookings", (req, res) => {
+  const newId = booking[booking.length - 1].id + 1
+  const { title, firstName, surname, email, roomId, checkInDate, checkOutDate } = req.body
+  const obj = { id: newId, ...req.body }
+  console.log(obj); if (!title || !firstName || !surname || !email || !roomId || !checkInDate || !checkOutDate) { res.status(400).send("missing value"); } else { res.status(201).json({ obj }) }
+})
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+// get a booking by id
+app.get("/bookings/:id", function (request, response) {
+  const idToFind = Number(request.params.id);
+  const booking = bookings.find((booking) => booking.id === idToFind);
+  response.status(200).send({ booking })
 });
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
