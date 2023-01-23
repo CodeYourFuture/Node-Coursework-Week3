2bbfe42fc59b81a84d28756bd9fa0413d97be699
michelle <mindyourphotosynthesis@gmail.com>
2023-01-23 20:55:32 +0000

    bookings: updated

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+const bookings = require('./bookings.json')
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
-});
+app.get('/', function (request, response) {
+  response.send('Hotel booking server.  Ask for /bookings, etc.')
+})
 
 // TODO add your routes and helper functions here
 
+app.get('/bookings', (req, res) => {
+  res.json(bookings)
+})
+
+app.get('/bookings/:id', (req, res) => {
+  const id = Number(req.params.id)
+  let bookingsCopy = bookings
+
+  res.json(bookingsCopy.filter((booking) => booking.id === id))
+})
+
+app.post(('/bookings', (req, res) => {
+  let body = req.body
+  console.log(body)
+}))
+
 const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+  console.log('Your app is listening on port ' + listener.address().port)
+})
