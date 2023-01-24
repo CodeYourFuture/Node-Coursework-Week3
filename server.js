943a4de74608c897231eefc9deb7db09abf3cd93
baharbazargan62 <baharbazargan62@gmail.com>
2023-01-24 10:30:59 +0000

    level 4 dependencies

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser=require("body-parser")
 const moment=require("moment")
+const validator=require("email-validator")
 const app = express();
 
 
