e3b81d622bf784cde124033b00eac495803899e4
fatih-celebi <celebifatih.c1@gmail.com>
2023-01-25 01:12:28 +0000

    Add Validation

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(cors());
 //Use this array as your (in-memory) data store.
 let bookings = require("./bookings.json");
 const { response } = require("express");
+const { hasSubscribers } = require("diagnostics_channel");
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
@@ -19,9 +20,47 @@ app.get("/bookings", function (req, res) {
 });
 
 app.post("/bookings", function (req, res) {
-  const newBooking = req.body;
-  bookings.push(newBooking);
-  res.send(newBooking);
+  const booking = req.body;
+  const {
+    id,
+    title,
+    firstName,
+    surname,
+    email,
+    roomId,
+    checkInDate,
+    checkOutDate,
+  } = booking;
+  const arrayOfBooking = Object.values(booking);
+  const isValid =
+    arrayOfBooking.every((value) => value !== undefined && value !== "") &&
+    !!id &&
+    !!title &&
+    !!firstName &&
+    !!surname &&
+    !!email &&
+    !!roomId &&
+    !!checkInDate &&
+    !!checkOutDate;
+
+  if (isValid) {
+    let newBooking = {
+      id: id,
+      title: title,
+      firstName: firstName,
+      surname: surname,
+      email: email,
+      roomId: roomId,
+      checkInDate: checkInDate,
+      checkOutDate: checkOutDate
+    };
+    bookings.push(newBooking);
+    res.send(
+      `Your booking has been successfully added ${JSON.stringify(newBooking)}`
+    );
+  } else {
+    res.status(400).send("Missing Info");
+  }
 })
 
 app.get("/bookings/:id", function (req, res) {
