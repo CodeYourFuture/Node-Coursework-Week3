55a17ca05d9b02dbed09bc128426658d8ebe7e38
levilu800b <levilu800b@gmail.com>
2023-01-27 15:04:57 +0000

    Level 1

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
 
@@ -17,6 +15,39 @@ app.get("/", function (request, response) {
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+app.get("/bookings", function (req, res) {
+  res.send(bookings);
+});
+
+app.get("/bookings/:id", function (req, res) {
+  const bookingId = +req.params.id;
+  const valid = bookings.some((booking) => booking.id === bookingId);
+  if (valid) {
+    const matched = bookings.find((booking) => booking.id === bookingId);
+    res.send(matched);
+  } else {
+    res.status(404).send("incorrect id");
+  }
+});
+
+app.post("/bookings", function (req, res) {
+  const newBooking = req.body;
+  newBooking.id = bookings.length + 1;
+  bookings.push(newBooking);
+  res.send(bookings);
+});
+
+app.delete('/bookings/:id', (req, res) => {
+	const bookingId = +req.params.id;
+	const valid = bookings.some((booking) => booking.id === bookingId);
+	if (valid) {
+		const filtered = bookings.filter((booking) => booking.id !== bookingId);
+		res.send(filtered);
+	} else {
+		res.status(404).send('incorrect id');
+	}
+});
+
+const listener = app.listen(5000 || process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
