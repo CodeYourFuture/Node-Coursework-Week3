b0fb160dc3a3d78270c97278aa6cb391015496fa
KawaManmi <kawamam2007@gmail.com>
2023-01-30 21:54:31 +0000

    added .gitignore

--- server.js
+++ server.js
@@ -146,3 +146,6 @@ const PORT = process.env.PORT || 5000;
 const listener = app.listen(PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+
+// just added .gitignore
