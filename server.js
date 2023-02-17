6967c0c4c51dc1c6d1233c84e18ff1332b2e136b
Anosh Malik <anoshmalik.am@gmail.com>
2023-02-17 00:35:30 +0000

    Level 1 complete

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+let bookings = require("./bookings.json");
 
 app.get("/", function (request, response) {
   response.send(bookings);
@@ -16,13 +16,33 @@ app.get("/", function (request, response) {
 
 // GET SPECIFIC
 app.get("/:id", function (request, response) {
-  response.send(bookings.filter(booking => booking.id==request.params.id));
+  response.send(bookings.filter((booking) => booking.id == request.params.id));
 });
 
+app.delete("/:id", function (requ, response) {
+  bookings.map((booking, index) => {
+    if (requ.params.id == booking.id) {
+      bookings.splice(index, 1);
+      console.log("Executing");
 
+    } else {
+      console.log("Not executing");
+    }
+  });
+  // response.send(`DELETED USER ${request.params.id}`);
+  response.json(bookings);
+});
+
+app.post("/", function (request, response) {
+  const body = request.body;
+  bookings.push(body);
+  console.log(body);
+  response.send(bookings);
+  // response.send(bookings);
+});
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(3000, function () {
+const listener = app.listen(5000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
