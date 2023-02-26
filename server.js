c57ac38460fad461c661b0b135592bd05029de41
Anosh Malik <anoshmalik.am@gmail.com>
2023-02-26 21:07:39 +0000

    Level 2 validation working. Coursework complete

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-// app.engine("html", require("ejs").renderFile);
 
 app.use(express.json());
 app.use(cors());
+app.set("view engine", "html");
+app.engine("html", require("ejs").renderFile);
+
+
 
 //Use this array as your (in-memory) data store.
 let bookings = require("./bookings.json");
@@ -28,7 +31,7 @@ app.get("/big", function (request, response) {
 
 // EXPERIMENT #3
 app.get("/boss", function (request, response) {
-  response.render(__dirname + "/bigboss.html");
+  response.render(__dirname + '/bigboss.html', {status: 'good'});
 });
 
 
@@ -37,9 +40,9 @@ app.get("/:id", function (request, response) {
   response.json(bookings.filter((booking) => booking.id == request.params.id));
 });
 
-app.delete("/:id", function (requ, response) {
+app.delete("/:id", function (request, response) {
   bookings.map((booking, index) => {
-    if (requ.params.id == booking.id) {
+    if (request.params.id == booking.id) {
       bookings.splice(index, 1);
       console.log("Executing");
 
@@ -52,8 +55,13 @@ app.delete("/:id", function (requ, response) {
 
 app.post("/", function (request, response) {
   const body = request.body;
-  bookings.push(body);
-  response.json(bookings);
+  if (body['id'] && body['title'] && body['firstName'] && body['surname'] && body['email'] && body['roomId'] && body['checkInDate'] && body['checkOutDate']) {
+    bookings.push(body);
+    response.json(bookings[bookings.length-1]);
+  } else { 
+    response.status(400).json("Incorrect details sent! Check your post method");
+
+  }
 });
 
 
