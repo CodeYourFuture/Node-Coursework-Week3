6108c71e7dd284a23146607b2766b198cc70aa29
Alex-Phillip <alexjoelphillip@gmail.com>
2023-04-03 16:35:13 +0100

    endpoint added: 1. Read one message specified by id

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const port = 3000
+
 //Use this array as your (in-memory) data store.
 const bookings = require('./bookings.json')
 
@@ -16,8 +18,24 @@ app.get('/', function (request, response) {
 // TODO add your routes and helper functions here
 
 // 1. Read all bookings
-app.get('/bookings', (req, res) => res.send(bookings))
+app.get('/bookings', (req, res) => {
+  res.send(bookings)
+})
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port)
+// 1. Read one booking, specified by an ID
+app.get('/bookings/:id', (req, res) => {
+  const id = req.params.id
+  console.log(id)
+  const booking = bookings.find((mes) => {
+    return mes.id.toString() === id
+  })
+  res.send(booking)
+})
+
+// const listener = app.listen(process.env.PORT, function () {
+//   console.log('Your app is listening on port ' + listener.address().port)
+// })
+
+const listener = app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
 })
