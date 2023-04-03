d115e0b367320c89f2fa0d7f3bac238045d6f56e
Alex-Phillip <alexjoelphillip@gmail.com>
2023-04-03 15:43:28 +0100

    endpoint added: 1.Read all bookings

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+const bookings = require('./bookings.json')
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
-});
+app.get('/', function (request, response) {
+  response.send('Hotel booking server.  Ask for /bookings, etc.')
+})
 
 // TODO add your routes and helper functions here
 
+// 1. Read all bookings
+app.get('/bookings', (req, res) => res.send(bookings))
+
 const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+  console.log('Your app is listening on port ' + listener.address().port)
+})
