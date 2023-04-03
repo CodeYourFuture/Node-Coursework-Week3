e9b1cb093c062c1ca86c6fe288d149f5c8848e28
Alex-Phillip <alexjoelphillip@gmail.com>
2023-04-03 22:21:03 +0100

    endpoint added: 1. Create a new booking

--- server.js
+++ server.js
@@ -17,31 +17,60 @@ app.get('/', function (request, response) {
 
 // TODO add your routes and helper functions here
 
+// 1. Creat a new booking
+app.post('/bookings', (req, res) => {
+  const booking = {
+    title: 'Miss',
+    firstName: 'Matilda',
+    surname: 'Spence',
+    email: 'matilda@example.com',
+    roomId: 6,
+    checkInDate: '2018-04-12',
+    checkOutDate: '2018-04-19',
+  }
+  if (!booking) {
+    res.send('error 404')
+  } else {
+    booking.id = 6
+    bookings.push(booking)
+    res.send(
+      `Booking added for ${booking.title} ${booking.firstName} ${booking.surname}`
+    )
+  }
+  console.log(bookings)
+})
+
 // 1. Read all bookings
 app.get('/bookings', (req, res) => {
   res.send(bookings)
 })
 
-// 1. Read one booking, specified by ID
+// 1. Read one booking, specified by an ID
 app.get('/bookings/:id', (req, res) => {
   const id = req.params.id
   console.log(id)
   const booking = bookings.find((mes) => {
     return mes.id.toString() === id
   })
-  res.send(booking)
+  if (!booking) {
+    req.send('error 404')
+  } else {
+    res.send(booking)
+  }
 })
 
-// 1. Delete one booking, specified by an ID
+// 1. Delete a booking, specified by an ID
 app.delete('/bookings/delete/:id', (req, res) => {
-  console.log(bookings)
   const id = req.params.id
   const booking = bookings.find((mes) => {
     return mes.id.toString() === id
   })
-  bookings.splice(booking, 1)
-  console.log(bookings)
-  res.send(`Message with id: ${id} has been deleted`)
+  if (!booking) {
+    res.send('error 404')
+  } else {
+    bookings.splice(booking, 1)
+    res.send(`Message with id: ${id} has been deleted`)
+  }
 })
 
 // const listener = app.listen(process.env.PORT, function () {
