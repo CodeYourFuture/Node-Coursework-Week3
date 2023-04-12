3bfdb4ccb2f9ebebdb4e6a3c117a540a1e2039d3
Lorena <lorenacapraru@gmail.com>
2023-04-12 20:12:23 +0100

    create and read

--- server.js
+++ server.js
@@ -13,8 +13,21 @@ app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
+//1.Create a new booking
+app.post("/bookings", function (request, response) {
+  const newBooking = request.body;
+  bookings.push(newBooking);
+  response.send(bookings);
+});
+
+//2.Read all bookings
+app.get("/bookings", function (request, response) {
+  response.send(bookings);
+});
+
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+// const listener = app.listen(process.env.PORT, function () {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
+app.listen(8080);
