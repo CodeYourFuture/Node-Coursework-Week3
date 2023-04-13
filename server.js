810597d177cf53d034640ea531b9a37d0845aba6
Bahare <itxbb09@gmail.com>
2023-04-13 13:46:11 +0100

    update Level -1

--- server.js
+++ server.js
@@ -12,19 +12,24 @@ const bookings = require("./bookings.json");
 //Create a new booking
 app.post("/bookings", (req, res) => {
   const newBooking = req.body;
+  //creating id for the new booking
   newBooking.id = Math.max(...bookings.map((booking) => booking.id), 0) + 1;
   bookings.push(newBooking);
   res.json(bookings);
 });
 //read all bookings
 app.get("/bookings", (req, res) => {
-  res.send(bookings);
+  res.json(bookings);
 });
 // Read one booking, specified by an ID
 app.get("/bookings/:id", (req, res) => {
   const bookingId = Number(req.params.id);
-  const filtered = bookings.filter((booking) => booking.id === bookingId);
-  res.send(filtered);
+  let filtered = bookings.find((booking) => booking.id === bookingId);
+  if (filtered) {
+    res.json(filtered);
+  } else {
+    res.status(404).send("the booking to be read cannot be found by id");
+  }
 });
 //Delete a booking, specified by an ID
 app.delete("/bookings/:id", (req, res) => {
@@ -32,8 +37,12 @@ app.delete("/bookings/:id", (req, res) => {
   const bookingIndex = bookings.findIndex(
     (booking) => booking.id === bookingId
   );
-  bookings.splice(bookingIndex, 1);
-  res.send(bookings);
+  if (bookingIndex === -1) {
+    res.status(404).send("the booking to be delete cannot be found by id");
+  } else {
+    bookings.splice(bookingIndex, 1);
+    res.json(bookings);
+  }
 });
 
 const listener = app.listen(process.env.PORT, function () {
