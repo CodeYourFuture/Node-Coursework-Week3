cfeaa9f4f5289b36110f32c4006affbcf6b9122a
Bahare <itxbb09@gmail.com>
2023-04-13 12:04:39 +0100

    Level-1 Done

--- server.js
+++ server.js
@@ -9,11 +9,32 @@ app.use(cors());
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+//Create a new booking
+app.post("/bookings", (req, res) => {
+  const newBooking = req.body;
+  newBooking.id = Math.max(...bookings.map((booking) => booking.id), 0) + 1;
+  bookings.push(newBooking);
+  res.json(bookings);
 });
-
-// TODO add your routes and helper functions here
+//read all bookings
+app.get("/bookings", (req, res) => {
+  res.send(bookings);
+});
+// Read one booking, specified by an ID
+app.get("/bookings/:id", (req, res) => {
+  const bookingId = Number(req.params.id);
+  const filtered = bookings.filter((booking) => booking.id === bookingId);
+  res.send(filtered);
+});
+//Delete a booking, specified by an ID
+app.delete("/bookings/:id", (req, res) => {
+  const bookingId = Number(req.params.id);
+  const bookingIndex = bookings.findIndex(
+    (booking) => booking.id === bookingId
+  );
+  bookings.splice(bookingIndex, 1);
+  res.send(bookings);
+});
 
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
