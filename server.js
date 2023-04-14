a28720f694c6d26a80fe7036e0a8e117bc22c001
elahemortazavi <emor5680@gmail.com>
2023-04-14 10:32:34 +0100

    route

--- server.js
+++ server.js
@@ -16,6 +16,51 @@ app.get("/", function (request, response) {
 });
 
 // TODO add your routes and helper functions here
+// Helper function to find a booking by ID
+function findBookingById(id) {
+  return bookings.find((booking) => booking.id == id);
+}
+
+// Helper function to generate a new booking ID
+function generateBookingId() {
+  return Math.max(...bookings.map((booking) => booking.id), 0) + 1;
+}
+
+function generateBookingroomId() {
+  return Math.max(...bookings.map((booking) => booking.roomId), 0) + 1;
+}
+
+// Route to create a new booking
+app.post("/bookings", function (req, res) {
+  const newBooking = req.body;
+  if (
+    !newBooking.title ||
+    !newBooking.firstName ||
+    !newBooking.surname ||
+    !newBooking.email ||
+    !newBooking.checkInDate ||
+    !newBooking.checkOutDate
+  ) {
+    res.status(400).send("Missing or empty booking property.");
+  } else if (!validator.isEmail(newBooking.email)) {
+    res.status(400).send("Invalid email address.");
+  } else {
+    const checkInDate = moment(newBooking.checkInDate, "YYYY-MM-DD");
+    const checkOutDate = moment(newBooking.checkOutDate, "YYYY-MM-DD");
+    if (
+      !checkInDate.isValid() ||
+      !checkOutDate.isValid() ||
+      !checkOutDate.isAfter(checkInDate)
+    ) {
+      res.status(400).send("Invalid date(s).");
+    } else {
+      newBooking.id = generateBookingId();
+      newBooking.roomId = generateBookingroomId();
+      bookings.push(newBooking);
+      res.status(201).send("you added new booking");
+    }
+  }
+});
 
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
