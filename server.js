d6fe46467d0683ade337f99fe819aa5e4c0aecbc
nataliizab <nataliia.zab@gmail.com>
2023-04-15 19:19:56 +0100

    Completed Level1

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
 
@@ -14,46 +13,96 @@ app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
-
-
 app.get("/bookings", function (request, response) {
-  response.status(200).send({bookings});
+  response.status(200).send({ bookings });
 });
 
+app.get("/bookings/search", function (request, response) {
+  const searchTerm = request.query.term.toLowerCase();
+  const matchingBookings = bookings.filter((booking) => {
+    return (
+      booking.firstName.toLowerCase().includes(searchTerm) ||
+      booking.surname.toLowerCase().includes(searchTerm) ||
+      booking.email.toLowerCase().includes(searchTerm) ||
+      booking.roomId.toString().toLowerCase().includes(searchTerm) ||
+      booking.checkInDate.toLowerCase().includes(searchTerm) ||
+      booking.checkOutDate.toLowerCase().includes(searchTerm)
+    );
+  });
 
+  if (matchingBookings.length === 0) {
+    return response.status(404).send({ error: "No matching bookings found" });
+  }
+  response.status(200).send({ bookings: matchingBookings });
+});
 
 app.get("/bookings/:id", function (request, response) {
   const idToFind = Number(request.params.id);
   const booking = bookings.find((booking) => booking.id === idToFind);
-  console.log(booking);
-  response.status(200).send(booking);
+  if (!booking) {
+    response
+      .status(404)
+      .send(`Booking with id ${request.params.id} is not found`);
+  } else {
+    response.status(200).send(booking);
+  }
 });
 
-// const listener = app.listen(process.env.PORT, function () {
-//   console.log("Your app is listening on port " + listener.address().port);
-// });
+app.delete("/bookings/:id", function (req, res) {
+  const idToDelete = parseInt(req.params.id);
+  const bookingIndex = bookings.findIndex(
+    (booking) => booking.id === idToDelete
+  );
+  if (bookingIndex === -1) {
+    return res.status(404).send({ error: "Booking not found" });
+  }
+  bookings.splice(bookingIndex, 1);
+  res.status(200).send(`The booking No. ${idToDelete} has been deleted`);
+});
+
+app.post("/bookings", function (request, response) {
+  const newBooking = {
+    id: bookings.length + 1, // a unique ID for the new booking
+    firstName: request.body.firstName,
+    surname: request.body.surname,
+    email: request.body.email,
+    roomId: parseInt(request.body.roomId),
+    checkInDate: request.body.checkInDate,
+    checkOutDate: request.body.checkOutDate,
+  };
+
+  // Validating the request body
+  if (
+    !newBooking.firstName ||
+    !newBooking.surname ||
+    !newBooking.email ||
+    isNaN(newBooking.roomId) ||
+    !newBooking.checkInDate ||
+    !newBooking.checkOutDate
+  ) {
+    return response.status(400).send({ error: "Invalid booking data" });
+  }
+  // // Checking if the room is available for the requested dates
+  // const overlappingBookings = bookings.filter((booking) => {
+  //   const isOverlapping =
+  //     newBooking.roomId === booking.roomId &&
+  //     newBooking.checkInDate < booking.checkOutDate &&
+  //     newBooking.checkOutDate > booking.checkInDate;
+  //   return isOverlapping;
+  // });
+
+  // if (overlappingBookings.length > 0) {
+  //   return response
+  //     .status(409)
+  //     .send({ error: "Room not available for the selected dates" });
+  // }
+
+  // Adding the new booking to the bookings array
+  bookings.push(newBooking);
+  response.status(201).send({ booking: newBooking });
+});
 
 const port = 3000;
 const listener = app.listen(port, function () {
   console.log("Your app is listening on port " + port);
 });
-
-
-
-
-
-// Use express.json() middleware to parse incoming JSON data.
-// Use cors() middleware to enable Cross-Origin Resource Sharing.
-// Require the bookings data by using the require() function and assigning it to a variable named bookings. The data is in a separate file named bookings.json.
-
-// Define the routes for handling the bookings:
-
-// For GET /bookings, return all bookings.
-// For GET /bookings/:id, return a single booking identified by its id.
-// For GET /bookings/search, return all bookings that match a search term passed as a query parameter.
-// For POST /bookings, create a new booking and return it.
-// For DELETE /bookings/:id, delete a booking identified by its id.
-// Start the server by calling the app.listen() method and passing in a port number.
-
-// Test the server by sending requests to each of the defined routes using Postman or any other RESTful API testing tool.
-
