7c1e2c0b27da0255cc180517f17ea898ae97b6ee
Hengameh M.Khani <afsooneday@gmail.com>
2023-04-15 13:17:56 +0100

    find-by-id

--- server.js
+++ server.js
@@ -1,9 +1,122 @@
+// const express = require("express");
+// const cors = require("cors");
+
+// const app = express();
+// const port = 3003;
+
+// app.use(express.json());
+// app.use(cors());
+
+// //Use this array as your (in-memory) data store.
+// const bookings = require("./bookings.json");
+
+// app.get("/", function (request, response) {
+//   response.send("Hotel booking server.  Ask for /bookings, etc.");
+// });
+
+// app.get("/booking", function (request, response) {
+//   response.json(bookings);
+// })
+
+// // Create a new booking
+// app.post("/bookinng",(request,response)=> {
+//   if(!request.body) {
+//     return response.status(400).send( {
+//       message: "Booking content can not be empty"
+
+//     });
+//   }
+// // Check if any property of the booking object is missing or empty
+// if(!request.body.name || !request.body.date || !request.body.room) {
+//   return response.status(400).send( {
+//     message: " Name, Date, and room are required"
+
+//   });
+// }
+
+// // TODO add your routes and helper functions here
+
+// // const listener = app.listen(process.env.PORT, function () {
+// //   console.log("Your app is listening on port " + listener.address().port);
+// // });
+
+// const booking = {
+//   "id": 6,
+//     "title": "Mrs",
+//     "firstName": "Heni",
+//     "surname": "MKH",
+//     "email": "Heni@example.com",
+//     "roomId": 6,
+//     "checkInDate": "20123-03-21",
+//     "checkOutDate": "2023-03-27"
+//   };
+
+// // Add the new booking to the array
+// bookinng.push(bookinng);
+
+// //Send the bookings array as a response
+// response.send(bookings);
+// });
+
+//  //Read all bookings
+//  app.get('/booking/:id',(req,res)=> {
+//   const id = parseInt(req.params.id);
+//   const booking = booking.find(b => b.id === id);
+
+//   //If booking is not found, return 404
+//   if (!booking) {
+//     return res.stutus(404).send({
+//       message: "Booking not found with id " + req.params.id
+//     });
+//   }
+
+//     //Send the booking as a response
+//     res.send(booking);
+
+//  });
+
+//  //Delete a booking, specified by an ID
+//  app.delete('/bookinng/:id',(req,res)=>{
+//   const id = parseint(req.params.id);
+//   const booking = booking.find(b => b.id === id);
+
+//    //If booking is not found, return 404
+//    if (!booking) {
+//     return res.stutus(404).send({
+//       message: "Booking not found with id" + req.params.id
+//     });
+//    }
+
+//  //Delete the booking
+//  const index = bookings.indexOf(booking);
+//  booking.splice(index,1);
+//  //Send the bookings array as a response
+//  res.send(bookings);
+// })
+
+// app.get('/booking/search',(req,res)=> {
+//   const date = req.quary.date;
+
+//   //Check if date is provided
+//   if (!date) {
+//     return res.status(400).send({
+//       message: "Date is required"
+//     });
+//   }
+//  //Filter the bookings array
+//  const bookingsSpanDate = booking.filter(b => b.date ==date);
+//  res.send(bookingsspanDate);
+
+// });
+
+// app.listen(port, function() {
+//   console.log("go to this port 3003")
+// });
+
 const express = require("express");
 const cors = require("cors");
 
-
 const app = express();
-const port = 3003;
 
 app.use(express.json());
 app.use(cors());
@@ -11,110 +124,35 @@ app.use(cors());
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
 
-
 app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+  response.send("Welcome to my API. Try /bookings");
 });
-app.get("/booking", function (request, response) {
-  response.send(bookings);
-})
 
-// Create a new booking
-app.post("/bookinng",(request,response)=> {
-  if(!request.body) {
-    return response.status(400).send( {
-      message: "Booking content can not be empty"
+app.get("/bookings", function (request, response) {
+  response.json(bookings);
+});
 
-    });
-  }
-// Check if any property of the booking object is missing or empty
-if(!request.body.name || !request.body.date || !request.body.room) {
-  return response.status(400).send( {
-    message: " Name, Date, and room are required"
+app.get("/bookings/:id", function (request, response) {
+  const id = Number(request.params.id);
 
+  // write logic to find a booking by its id
+  const booking = bookings.find((value) => {
+    return value.id === id;
   });
-}
 
-
-
-// TODO add your routes and helper functions here
-
-
-// const listener = app.listen(process.env.PORT, function () {
-//   console.log("Your app is listening on port " + listener.address().port);
-// });
-  
-const booking = { 
-  "id": 6,
-    "title": "Mrs",
-    "firstName": "Heni",
-    "surname": "MKH",
-    "email": "Heni@example.com",
-    "roomId": 6,
-    "checkInDate": "20123-03-21",
-    "checkOutDate": "2023-03-27"
-  };
-  
-
-// Add the new booking to the array
-bookinng.push(bookinng);
-
-
-//Send the bookings array as a response
-response.send(bookings);
+  if (!booking){
+    response.status(404).json({message: "Booking not found"});
+  }else {
+    response.json(booking);
+  }
 });
 
- //Read all bookings
- app.get('/booking/:id',(req,res)=> {
-  const id = parseInt(req.params.id);
-  const booking = booking.find(b => b.id === id);
 
-  //If booking is not found, return 404
-  if (!booking) {
-    return res.stutus(404).send({
-      message: "Booking not found with id " + req.params.id
-    });
-  }
 
-    //Send the booking as a response
-    res.send(booking);
 
- });
 
- //Delete a booking, specified by an ID
- app.delete('/bookinng/:id',(req,res)=>{
-  const id = parseint(req.params.id);
-  const booking = booking.find(b => b.id === id);
 
-   //If booking is not found, return 404
-   if (!booking) {
-    return res.stutus(404).send({
-      message: "Booking not found with id" + req.params.id
-    });
-   }
 
- //Delete the booking
- const index = bookings.indexOf(booking);
- booking.splice(index,1);
- //Send the bookings array as a response
- res.send(bookings);
-})
-
-app.get('/booking/search',(req,res)=> {
-  const date = req.quary.date;
-
-  //Check if date is provided
-  if (!date) {
-    return res.status(400).send({
-      message: "Date is required"
-    });
-  }
- //Filter the bookings array
- const bookingsSpanDate = booking.filter(b => b.date ==date);
- res.send(bookingsspanDate);
-
-});
-
-app.listen(port, function() {
-  console.log("go to this port 3003")
+const listener = app.listen(3001, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
