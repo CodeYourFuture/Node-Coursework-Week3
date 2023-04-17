9c42e24f9f9553541a090d77256d9a4e7491f8aa
BoshraM <gbboshra@gmail.com>
2023-04-17 16:51:42 +0100

    the post reqest completed

--- server.js
+++ server.js
@@ -15,37 +15,37 @@ app.get("/", function (request, response) {
 });
 
 // TODO add your routes and helper functions here
- app.get("/bookings", (request,response) => {
-  !bookings ? response.status(400).json({error:true, message:"there is an error!"}) :
-  response.status(200).json({success:true, data:bookings })
- });
+app.get("/bookings", (request, response) => {
+  !bookings ? response.status(400).json({ error: true, message: "there is an error!" }) :
+    response.status(200).json({ success: true, data: bookings })
+});
 
- app.get("/bookings/:id", (request,response) => {
-  let bookingId= request.params.id
+app.get("/bookings/:id", (request, response) => {
+  let bookingId = request.params.id
 
-  if(!bookingId){
-    return response.status(400).json({error:true, message:"there is an error!"})
+  if (!bookingId) {
+    return response.status(400).json({ error: true, message: "there is an error!" })
   };
 
-  let findingBooking=bookings.find(booking => booking.id===+bookingId)
+  let findingBooking = bookings.find(booking => booking.id === +bookingId)
 
-  !findingBooking ?response.status(404).json({success:false, message:"there is no booking with this id"}) ://which error status is correct for this kind of response??????
-  response.status(200).json({success:true, data:findingBooking })
- });
+  !findingBooking ? response.status(404).json({ success: false, message: "there is no booking with this id" }) ://which error status is correct for this kind of response??????
+    response.status(200).json({ success: true, data: findingBooking })
+});
 
- app.delete("/bookings/:id", (request, response) => {
+app.delete("/bookings/:id", (request, response) => {
   let bookingId = request.params.id;
-  if (!bookingId){
-    return response.status(400).json({error:true, message:"there is an error!"})
+  if (!bookingId) {
+    return response.status(400).json({ error: true, message: "there is an error!" })
   }
   let bookingToDelete = bookings.find(booking => booking.id === +bookingId);
-  let indexOfMessageToDElete= bookings.indexOf(bookingToDelete);
-  bookings.splice(indexOfMessageToDElete,1)
-  response.status(200).json({success:true, data:bookings })
- });
+  let indexOfMessageToDElete = bookings.indexOf(bookingToDelete);
+  bookings.splice(indexOfMessageToDElete, 1)
+  response.status(200).json({ success: true, data: bookings })
+});
 
-app.post("/bookings", (request, response) =>{
-  const { id, title, firstName, surname, email, roomId, checkInDate, checkOutDate} = request.body;
+app.post("/bookings", (request, response) => {
+  const { id, title, firstName, surname, email, roomId, checkInDate, checkOutDate } = request.body;
   const newBooking = {
     id: bookings.length + 1,
     title,
@@ -56,9 +56,23 @@ app.post("/bookings", (request, response) =>{
     checkInDate,
     checkOutDate,
   };
-  
+
+  if (
+    !newBooking.title ||
+    !newBooking.firstName ||
+    !newBooking.surname ||
+    !newBooking.email ||
+    !newBooking.roomId ||
+    !newBooking.checkInDate ||
+    !newBooking.checkOutDate
+  ) {
+    return response
+      .status(400)
+      .json({ message: "Please fill in all required sections" });
+  }
+
   bookings.push(newBooking)
-  response.status(200).json({success:true , data: bookings})
+  response.status(200).json({ success: true, data: bookings })
 })
 
 const listener = app.listen(3001, function () {
