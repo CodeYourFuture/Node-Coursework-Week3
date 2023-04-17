4fb7c8b67a2ee7489ee1ac7bab078f53423547a9
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-04-17 19:37:56 +0100

    Level 1 Challenge - make the booking server - done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const port = 3030;
 
 const app = express();
 
@@ -9,12 +10,81 @@ app.use(cors());
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+//for creating new Id for new booking
+function getNewUniqueId(array) {
+  let newId;
+  do {
+    newId = Math.floor(Math.random() * 1000) + 1; // generate a random number between 1 and 1000
+  } while (array.some((obj) => obj.id === newId)); // check if the id already exists in the array
+  return newId;
+}
+
+// Read all bookings
+app.get("/bookings", function (req, res) {
+  res.status(200).send(bookings);
 });
 
-// TODO add your routes and helper functions here
+// Read one booking, specified by an ID
+app.get("/bookings/:id", function (req, res) {
+  const searchId = Number(req.params.id);
+  const bookingWithSearchId = bookings.find(
+    (booking) => booking.id === searchId
+  );
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+  if (!bookingWithSearchId) {
+    return res.status(404).send("Booking not found");
+  }
+
+  // If the booking is found, return it to the client
+  return res.status(200).send(bookingWithSearchId);
+});
+
+// Create a new booking
+app.post("/bookings", function (req, res) {
+  const {
+    title,
+    firstname,
+    surname,
+    email,
+    roomId,
+    checkInDate,
+    checkOutDate,
+  } = req.body;
+
+  const newBooking = {
+    id: getNewUniqueId(bookings),
+    title: title,
+    firstname: firstname,
+    surname: surname,
+    email: email,
+    roomId: roomId,
+    checkInDate: checkInDate,
+    checkOutDate: checkOutDate,
+  };
+
+  bookings.push(newBooking);
+
+  res.status(201).send(bookings);
+});
+
+// Delete a booking, specified by an ID
+app.delete("/bookings/:id", function (req, res) {
+  const searchId = Number(req.params.id);
+  const bookingWithSearchId = bookings.find(
+    (booking) => booking.id === searchId
+  );
+
+  if (!bookingWithSearchId) {
+    return res.status(404).send("Booking not found");
+  } else {
+    const index = bookings.indexOf(bookingWithSearchId);
+    console.log("index", index);
+    bookings.splice(index, 1);
+    // If the booking is found, return updated array with bookings
+    return res.status(200).send(bookings);
+  }
+});
+
+app.listen(port, () => {
+  console.log("Listening to the port 3030...");
 });
