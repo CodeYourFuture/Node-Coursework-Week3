f6d8cc005a21b08e844f36042e785fc1255b87a5
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-04-19 19:20:12 +0100

    Level one done

--- server.js
+++ server.js
@@ -7,14 +7,67 @@ app.use(express.json());
 app.use(cors());
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+let bookings = require("./bookings.json");
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
+//set an Id counter
+let idCounter = 6;
+
+//Create a new booking
+app.post("/bookings", function (request, response) {
+  const newBooking = {
+    id: idCounter++,
+    roomId: request.body.roomId,
+    title: request.body.title,
+    firstName: request.body.firstName,
+    surname: request.body.surname,
+    email: request.body.email,
+    checkInDate: request.body.checkInDate,
+    checkOutDate: request.body.checkOutDate,
+  };
+  console.log(request.body);
+  response.json(newBooking);
+  bookings.push(newBooking);
+});
+//Read all bookings
+app.get("/bookings", function (request, response) {
+  response.json(bookings);
+});
+
+//Read one booking
+
+function idFromRequest(request) {
+  return parseInt(request.params.id);
+}
+function findBookingById(id) {
+  return bookings.find((element) => element.id === id);
+}
+app.get("/bookings/:id", function (request, response) {
+  let id = idFromRequest(request);
+  let foundBooking = findBookingById(id);
+  if (foundBooking) {
+    response.json(foundBooking);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/bookings/:id", function (request, response) {
+  let id = idFromRequest(request);
+  let foundBooking = findBookingById(id);
+  if (foundBooking) {
+   bookings = bookings.filter((e) => e.id !== id);
+   response.json(bookings)
+  } else {
+    response.status(404).end();
+  }
+});
+
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
