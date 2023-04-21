34b55a479a3d701aeab9928a223721a90b7d20f4
monahvi <nahvi.moghaddam@gmail.com>
2023-04-21 12:29:04 +0100

    .

--- server.js
+++ server.js
@@ -7,14 +7,79 @@ app.use(express.json());
 app.use(cors());
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+let bookings = require("./bookings.json");
 
 app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+  response.send("Welcome to my API. Try /bookings");
 });
 
-// TODO add your routes and helper functions here
+//get bookings:
+app.get("/bookings", function (request, response) {
+  response.json(bookings);
+});
 
-const listener = app.listen(process.env.PORT, function () {
+//get by id:
+app.get("/bookings/:id", function (request, response) {
+  const id = Number(request.params.id);
+
+  const booking = bookings.find((value) => {
+    return value.id === id;
+  });
+
+  if (!booking) {
+    response.status(404).json({ message: "booking not found" });
+  } else {
+    response.json(booking);
+  }
+});
+
+//post:
+app.post("/bookings", function (request, response) {
+  const booking = request.body;
+  console.log(booking);
+  if (
+    !booking.title ||
+    !booking.firstName ||
+    !booking.surname ||
+    !booking.email ||
+    !booking.roomId ||
+    !booking.checkInDate ||
+    !booking.checkOutDate
+  ) {
+    response.status(400).json({ message: "invalid booking" });
+    return;
+  }
+
+  booking.id = bookings.length + 1;
+
+  bookings.push(request.body);
+
+  response.json(bookings);
+});
+
+//delete:
+app.delete("/bookings/:id", function (request, response) {
+  const id = Number(request.params.id);
+
+  //delete a booking  from bookings array by id:
+  const booking = bookings.find((value) => {
+    return value.id === id;
+  });
+
+  if (!booking) {
+    response.status(404).json({ message: "booking not found" });
+  } else {
+    const index = bookings.indexOf(booking);
+    bookings.splice(index, 1);
+
+    response.json(bookings);
+  }
+});
+
+//to get all ids respectively:
+// app.get("/bookings/ids", function (request, response) {
+//   response.json(bookings.map((booking) => booking.id));
+// });
+const listener = app.listen(3001, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
