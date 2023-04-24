3f58a8fc71f16bc499be8473b4bf479e03440f69
Olena Reukova <kronverk01@gmail.com>
2023-04-24 01:43:45 +0100

    Level 1
    API
    1. Creates a new booking
    1. Reads all bookings
    1. Reads one booking, specified by an ID
    1. Deletes a booking, specified by an ID
    
    If the booking to be read cannot be found by id, return a 404.
    
    If the booking for deletion cannot be found by id, return a 404.
    
    All booking content is passed as JSON.
    
    Using "Correct Routes" from the table

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const port = 3000;
 
 const app = express();
 
@@ -18,6 +19,64 @@ app.get("/", function (request, response) {
 //Level 1 Challenge - make the booking server
 //1. Created a new booking
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+app.post("/bookings", function (req, res) {
+  const bookingData = req.body;
+  const newBooking = {
+    ...bookingData,
+    id: bookings.length + 1,
+  };
+  bookings.push(newBooking);
+  res.json({ newBooking });
+});
+
+//Read all bookings
+app.get("/bookings", function (req, res) {
+  res.json({ bookings });
+  console.log(bookings);
+});
+
+//Read one booking, specified by an ID
+
+app.get("/bookings/:id", function (req, res) {
+  // req.params.id will match the value in the url after /bookings/
+  console.log(req.params.id);
+  // now we can use the value for req.params.id to find the booking requested
+  // find id in an array
+  //res.json(bookings[parseInt(req.params.id) - 1]);  it is one way of solution
+  const idToFind = Number(req.params.id);
+  const booking = bookings.find((booking) => booking.id === idToFind);
+
+  //If the booking to be read cannot be found by id, return a 404.
+  if (booking === undefined) {
+    res
+      .status(404)
+      .json({ message: `Couldn't find the booking for id: ${idToFind}` });
+  }
+
+  res.json({ booking });
+});
+
+//Delete a booking, specified by an ID
+app.delete("/bookings/:id", function (req, res) {
+  const idToDelete = Number(req.params.id);
+  const indexToDelete = bookings.findIndex(
+    (booking) => booking.id === idToDelete
+  );
+
+  //If the booking for deletion cannot be found by id, return a 404.
+
+  if (indexToDelete === -1) {
+    res
+      .status(404)
+      .json({ message: `Couldn't find the booking for id: ${indexToDelete}` });
+  }
+
+  const [deleteBooking] = bookings.splice(indexToDelete, 1);
+  res.json({ deleteBooking });
+});
+
+//All booking content should be passed as JSON - done
+
+app.listen(port, () => {
+  console.log("Listening to the port 3000...");
 });
