9c6970e523f9d4235190b531a50e32507e53df17
Gayle Ngozi <gaylengozi@gmail.com>
2023-05-03 15:51:36 +0100

    Levels 1-5 completed

--- server.js
+++ server.js
@@ -1,20 +1,108 @@
 const express = require("express");
 const cors = require("cors");
 
+const validator = require("email-validator");
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
+const welcomeMessage = "Hotel booking server. Ask for /bookings, etc.";
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+let bookings = require("./bookings.json");
 
-app.get("/", function (request, response) {
-  response.send("Hotel booking server.  Ask for /bookings, etc.");
+app.get("/welcome", function (request, response) {
+  response.send(welcomeMessage);
+});
+
+app.get("/bookings", function (request, response) {
+  response.json(bookings);
+});
+
+app.get("/bookings/search", function (request, response) {
+  const searchItems = request.query.text;
+  const foundItems = bookings.filter(
+    (item) =>
+      item.firstName.toLowerCase().includes(searchItems.toLowerCase()) ||
+      item.surname.toLowerCase().includes(searchItems.toLowerCase()) ||
+      item.email.toLowerCase().includes(searchItems.toLowerCase())
+  );
+  response.json(foundItems);
+});
+
+
+
+app.get("/bookings/:id", function (request, response) {
+  const bookingsId = request.params.id;
+  const oneBooking = bookings.find(
+    (eachBooking) => eachBooking.id === parseInt(bookingsId)
+  );
+  oneBooking
+    ? response.json(oneBooking)
+    : response.json({ message: "booking not found" });
+});
+
+
+app.delete("/bookings/:id", function (request, response) {
+  const bookingsId = request.params.id;
+  const oneBooking = bookings.find(
+    (eachBooking) => eachBooking.id === parseInt(bookingsId)
+  );
+  if (oneBooking) {
+    bookings = bookings.filter(
+      (eachBooking) => eachBooking.id === parseInt(bookingsId)
+    );
+    response.json({ message: "booking deleted" });
+  } else {
+    response.json({ message: "booking not found" });
+  }
 });
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+app.post("/bookings", function (request, response) {
+  const currentDate = new Date();
+  const date = currentDate.toLocaleDateString();
+  const time = currentDate.toLocaleTimeString("en-UK", { hour12: false });
+  const timeStamp = date + " @" + time;
+
+  const NewBooking = {
+    id: bookings.length + 1,
+    title: request.body.title,
+    firstName: request.body.firstName,
+    surname: request.body.surname,
+    email: request.body.email,
+    roomId: Math.floor(Math.random() * 100),
+    checkInDate: request.body.checkInDate,
+    checkOutDate: request.body.checkOutDate,
+    BookedOn: request.body.timeStamp,
+  };
+
+  if (
+    !NewBooking.firstName ||
+    !NewBooking.surname ||
+    !NewBooking.email ||
+    !NewBooking.checkInDate ||
+    !NewBooking.checkOutDate ||
+    !NewBooking.title
+  ) {
+    return response
+      .status(400)
+      .json({ message: "Please fill all required areas" });
+  }
+  if (!validator.validate(NewBooking.email)){
+    return response
+      .status(400)
+      .json({ message: "Please enter a valid email" });
+  }
+
+  bookings.push(NewBooking);
+  response.json(bookings);
+});
+
+
+const listener = app.listen(PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
