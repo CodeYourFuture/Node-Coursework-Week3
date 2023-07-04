ee46e18fec197edbe8c1533007ea753161d337ad
Mpanasetckiy <maxbuzz8694@gmail.com>
2023-07-04 21:25:35 +0100

    chore(package.json): update start script to use 'node' instead of 'nodemon' chore(package.json): add 'build' script with a dummy command feat(bookings-routes.js): add DELETE endpoint for deleting bookings by ID

--- routes/bookings-routes.js
+++ routes/bookings-routes.js
@@ -13,4 +13,15 @@ router.post("/bookings", (req, res, next) => {
   res.json({ message: "Data received successfully" });
 });
 
+router.delete("/bookings/:pid", (req, res, next) => {
+  const bookingId = req.params.pid;
+  const index = bookings.findIndex((booking) => booking.id === +bookingId);
+  if (index !== -1) {
+    bookings.splice(index, 1);
+    res.sendStatus(200); // Send a 200 OK status code
+  } else {
+    res.sendStatus(404); // Send a 404 Not Found status code if booking not found
+  }
+});
+
 module.exports = router;
