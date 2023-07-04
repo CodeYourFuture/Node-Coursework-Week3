3b667302509401778f68bd946e354623feaf5a2e
Mpanasetckiy <maxbuzz8694@gmail.com>
2023-07-04 19:22:01 +0100

    chore: add .env and node_modules to .gitignore chore(package.json): update start script to use nodemon feat(package.json): add cors and nodemon as dependencies feat(server.js): add support for process.env.PORT environment variable

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get("/", function (request, response) {
 
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server is live on port: " + (process.env.PORT || 5000));
 });
